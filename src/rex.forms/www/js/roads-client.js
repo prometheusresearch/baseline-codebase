af0dcaeebc474f57c0c8bf52162d25eecbef7654
Dmitry Druganov <drud@prometheusresearch.com>
2012-08-17 19:32:55 +0200

    Fixed position of buttons

--- src/rex.forms/www/js/roads-client.js
+++ src/rex.forms/www/js/roads-client.js
@@ -114,8 +114,8 @@ function ROADS(o) {
 
     function getRandomStr(len) {
         var text = "";
-        var possible = 
-                "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var possible =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
         for(var i = 0; i < len; i++)
             text += possible.charAt(Math.floor(Math.random() * possible.length));

39a67d607bdb0edee5133ec394cab7c6d6c9887a
Kirill Simonov <xi@resolvent.net>
2013-02-19 10:49:41 -0600

    rexsetup -> rex.setup

--- src/rex.setup/setup.py
+++ src/rex.setup/setup.py
@@ -3,29 +3,30 @@
 #
 
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
-    name='rexsetup',
+    name='rex.setup',
     version = "0.1.1",
     description="Distutils extension for the Rex platform",
     long_description=open('README', 'r').read(),
-    packages=['rexsetup'],
     package_dir={'': 'src'},
+    packages=find_packages('src'),
+    namespace_packages=['rex'],
     entry_points={
         'distutils.setup_keywords': [
-            'rex_data = rexsetup:check_data',
-            'rex_prefix = rexsetup:check_prefix',
-            'rex_load = rexsetup:check_load',
+            'rex_data = rex.setup:check_data',
+            'rex_prefix = rex.setup:check_prefix',
+            'rex_load = rex.setup:check_load',
         ],
         'distutils.commands': [
-            'install_rex = rexsetup:install_rex',
-            'develop_rex = rexsetup:develop_rex'],
+            'install_rex = rex.setup:install_rex',
+            'develop_rex = rex.setup:develop_rex'],
         'egg_info.writers': [
-            'rex_data.txt = rexsetup:write_data_txt',
-            'rex_prefix.txt = rexsetup:write_prefix_txt',
-            'rex_load.txt = rexsetup:write_load_txt',
+            'rex_data.txt = rex.setup:write_data_txt',
+            'rex_prefix.txt = rex.setup:write_prefix_txt',
+            'rex_load.txt = rex.setup:write_load_txt',
         ],
     },
 )

86c5add0f05cfab89398cfbf17335b56568463de
Jonathan McCall <jonathan@prometheusresearch.com>
2013-02-22 11:17:37 -0500

    Finalize tag 0.1.2 for rex.forms

--- src/rex.forms/setup.py
+++ src/rex.forms/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rex.forms',
-      version = "0.1.1",
+      version = "0.1.2",
       description="Your description here",
       setup_requires=['rexsetup'],
       # Uncomment next 2 lines if you are adding any python code

282f9b28bb0e1ce5f39fecabf550b29738bd96cf
Oleksiy Golovko <zindel@prometheusresearch.com>
2013-03-13 19:29:44 +0100

    bump version

--- src/rex.instrument/setup.py
+++ src/rex.instrument/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rex.instrument',
-      version = "0.1.0",
+      version = "0.1.1rc1",
       description="Instrument/Assessment model",
       packages=find_packages('src'),
       package_dir={'': 'src'},

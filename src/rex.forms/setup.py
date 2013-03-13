1a2c6b37f4d3b9395306ec3f4cb5e1dfea27d404
Jonathan McCall <jonathan@prometheusresearch.com>
2013-03-13 16:55:11 -0400

    Updating to version 0.1.5rc2 and adding htsql as a dependency.

--- src/rex.forms/setup.py
+++ src/rex.forms/setup.py
@@ -2,22 +2,23 @@
 from setuptools import setup, find_packages
 
 setup(name='rex.forms',
-      version = "0.1.5rc1",
+      version="0.1.5rc2",
       description="Your description here",
       setup_requires=['rexsetup'],
       # Uncomment next 2 lines if you are adding any python code
       packages=find_packages('src'),
       package_dir={'': 'src'},
-      include_package_data = True,
+      include_package_data=True,
       namespace_packages=['rex'],
-      install_requires=['rexrunner', 
-          'rex.common>=0.1.0',
-          'rex.ext>=0.1.0',
-          'rex.instrument>=0.1.0',
-      ],
+      install_requires=['rexrunner',
+                        'rex.common>=0.1.0',
+                        'rex.ext>=0.1.0',
+                        'rex.instrument>=0.1.0',
+                        'htsql>=2.3.4',
+                        ],
 
       www_prefix='/rex.forms',
       www_dir='www',
       www_module='rex.forms',
       www_settings='settings.yaml',
-)
+      )

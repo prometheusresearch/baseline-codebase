14715901ff791a3be6e738b281a9c957c67ca6f2
Kirill Simonov <xi@resolvent.net>
2013-06-17 16:18:54 -0500

    Replaces `cogs.local.py` with `rex demo/doc/test`.

--- src/rex.core/demo/rex.core_demo/setup.py
+++ src/rex.core/demo/rex.core_demo/setup.py
@@ -1,5 +1,25 @@
 
 from setuptools import setup, find_packages
+from distutils.core import Command
+
+class demo(Command):
+
+    description = "show how to get a list of packages and settings"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from rex.core import Rex, get_packages, get_settings
+        demo = Rex('rex.core_demo')
+        print "demo:", demo
+        with demo:
+            print "get_packages():", get_packages()
+            print "get_settings():", get_settings()
 
 setup(
     name='rex.core_demo',
@@ -14,6 +34,7 @@ setup(
     install_requires=[
         'rex.core',
     ],
+    cmdclass={'demo': demo},
     rex_init='rex.core_demo',
     rex_static='static',
 )

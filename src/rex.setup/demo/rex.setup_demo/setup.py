3e3e88d3b559f69f145b1bde43cdb6abe47a58fa
Kirill Simonov <xi@resolvent.net>
2013-06-17 16:16:54 -0500

    Replaced `cogs.local.py` with `rex demo/doc/test`.

--- src/rex.setup/demo/rex.setup_demo/setup.py
+++ src/rex.setup/demo/rex.setup_demo/setup.py
@@ -1,5 +1,23 @@
 
 from setuptools import setup, find_packages
+from distutils.core import Command
+
+class demo(Command):
+
+    description = "show how to load Rex metadata"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import pkg_resources
+        dist = pkg_resources.get_distribution('rex.setup_demo')
+        print "rex_init.txt:", dist.get_metadata('rex_init.txt')
+        print "rex_static.txt:", dist.get_metadata('rex_static.txt')
 
 setup(
     name='rex.setup_demo',
@@ -12,6 +30,7 @@ setup(
     packages=find_packages('src'),
     namespace_packages=['rex'],
     setup_requires=['rex.setup'],
+    cmdclass={'demo': demo},
     rex_init='rex.setup_demo',
     rex_static='static',
 )

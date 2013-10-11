28f12eb53b91a51699849a64c0552d66cd6823eb
Kirill Simonov <xi@resolvent.net>
2013-10-11 13:09:25 -0500

    Updated NEWs and bumped the version number.

--- src/rex.setup/setup.py
+++ src/rex.setup/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.setup',
-    version = "0.9.9",
+    version = "1.0.0",
     description="Distutils extension for the RexDB platform",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

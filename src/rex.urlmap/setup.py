93505c1fb6c84e8e490a87addcb40a5661e832f4
Kirill Simonov <xi@resolvent.net>
2013-11-20 12:23:41 -0600

    Set the release date.

--- src/rex.urlmap/setup.py
+++ src/rex.urlmap/setup.py
@@ -22,7 +22,7 @@ setup(
         'rex.setup >=1.0, <2',
     ],
     install_requires=[
-        'rex.core >=1.0, <2',
+        'rex.core >=1.1, <2',
         'rex.web >=1.1, <2',
     ],
     rex_init='rex.urlmap',

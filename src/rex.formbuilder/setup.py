e159c258b45537179b853db68b94a12f6e27b459
Owen McGettrick <owen@prometheusresearch.com>
2013-12-16 12:25:07 -0500

    bump version number

--- src/rex.formbuilder/setup.py
+++ src/rex.formbuilder/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rex.formbuilder',
-      version='1.0.1',
+      version='1.0.2',
       description="Your description here",
       entry_points = {
           'rdoma.modules': ['rex_formbuilder = rex.formbuilder.deploy'],

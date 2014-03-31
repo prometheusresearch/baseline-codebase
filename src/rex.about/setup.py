2e02f7291e6b6b685445b5a4dbae270f2e8b045a
Dmitry Druganov <drud@prometheusresearch.com>
2014-03-31 15:21:27 +0200

    fixed dependency

--- src/rex.about/setup.py
+++ src/rex.about/setup.py
@@ -17,7 +17,7 @@ setup(
     ],
     install_requires=[
         'rex.urlmap >= 1.0.1, <3',
-        'rex.ui >=1.0.1, <2',
+        'rex.ui >=1.0.12, <2',
     ],
     rex_init='rex.about',
     rex_static='static',

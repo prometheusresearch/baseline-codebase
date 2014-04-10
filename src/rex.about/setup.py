c0a2a95645651f71e31316733c6a38f2d45077d7
Phil Langthorne <phil@prometheusresearch.com>
2014-04-10 17:17:22 -0400

    bumping version

--- src/rex.about/setup.py
+++ src/rex.about/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.about',
-    version='0.0.3',
+    version='0.0.4',
     description='Licensing and dependent works declaration',
     long_description=open('README.rst', 'r').read(),
     maintainer='Prometheus Research, LLC',

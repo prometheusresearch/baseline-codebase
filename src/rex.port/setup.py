95bea851afbeb60ad2104be9060ba6190fc8add5
Kirill Simonov <xi@resolvent.net>
2014-05-05 15:10:42 -0500

    Depend upon rex.db>=2.1.

--- src/rex.port/setup.py
+++ src/rex.port/setup.py
@@ -24,7 +24,7 @@ setup(
     install_requires=[
         'rex.core >=1.6, <2',
         'rex.web >=2.1, <3',
-        'rex.db >=2.0, <3',
+        'rex.db >=2.1, <3',
     ],
     rex_init='rex.port',
     rex_static='static',

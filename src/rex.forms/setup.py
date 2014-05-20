cd531530604e744be4f96006fcefa79cc5028785
Jason Simeone <jason@prometheusresearch.com>
2014-05-20 11:44:39 -0400

    Version bump.

--- src/rex.forms/setup.py
+++ src/rex.forms/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.forms',
-    version='0.11.0',
+    version='0.11.1',
     description="RexForms client Javascript code & templates",
     long_description=open('README.rst', 'r').read(),
     maintainer='Prometheus Research, LLC',

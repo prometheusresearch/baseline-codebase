21421dd05958ae295961b3d4af2aabc6ae3f29e9
Alex Voronoy <alex@prometheusresearch.com>
2014-06-09 14:06:43 +0300

    Bumped revision

---
+++ src/rex.expression/NEWS.rst
@@ -0,0 +1,13 @@
+*****************************
+  Rex.expression Change Log
+*****************************
+
+1.1.0
+=====
+
+- Added a way to use rex.expression as a bower component.
+
+1.0.0
+=====
+
+- Added rexl code from rex.common.
--- src/rex.expression/setup.py
+++ src/rex.expression/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rex.expression',
-      version = "1.0.0",
+      version = "1.1.0",
       description="JavaScript library to parse HTSQL expressions",
       setup_requires=['rex.setup >= 1.1, <2'],
 #      packages=find_packages('src'),

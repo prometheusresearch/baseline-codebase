46029a0ebb954346d52e9552b6b8e98224b3c865
Dmitry Druganov <drud@prometheusresearch.com>
2014-06-16 18:29:41 +0200

    bumping version

--- src/rex.formbuilder/setup.py
+++ src/rex.formbuilder/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.formbuilder',
-    version='2.0.2',
+    version='2.0.3',
     description='A GUI for constructing RexAcquire Forms',
     long_description=open('README.rst', 'r').read(),
     maintainer='Prometheus Research, LLC',

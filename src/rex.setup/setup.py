acb1a49a7da893f0111051ae7975c5b6aed47fec
Kirill Simonov <xi@resolvent.net>
2014-07-01 09:44:43 -0500

    Making a patch release.

--- src/rex.setup/setup.py
+++ src/rex.setup/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.setup',
-    version = "1.1.0",
+    version = "1.1.1",
     description="Distutils extension for the RexDB platform",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

f0486cfe7e607b7929af4738278433b8f8ac13cd
Andrey Popp <8mayday@gmail.com>
2014-08-20 16:31:02 +0400

    DataPreloader: component which automates <Preloader /> rendering for data components.
    Example:
    
        <DataPreloader data={this.props.data}>
          <Grid data={this.props.data} />
        </DataPreloader>
    
    Will only render `<Grid />` if `this.props.data` is not in `updating` state,
    otherwise it renders `<Preloader />`.
    
    Another use case is:
    
        <DataPreloader
          data={this.props.data}
          renderer={this.renderGrid}
          />
    
    Which is similar to the first one but will call a function `renderer` in case
    data is available. This is useful when you want to render all the data inside a
    single component.

---
+++ src/rex.widget/static/js/lib/DataPreloader.js
@@ -0,0 +1,31 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+
+var React      = require('react');
+var PropTypes  = React.PropTypes;
+var Preloader  = require('./Preloader');
+
+var DataPreloader = React.createClass({
+
+  propTypes: {
+    data: PropTypes.object,
+    caption: PropTypes.string,
+    children: PropTypes.component,
+    renderer: PropTypes.func
+  },
+
+  render() {
+    var {updating} = this.props.data;
+    if (updating) {
+      return <Preloader caption={this.props.caption} />;
+    } else if (this.props.children) {
+      return React.Children.only(this.props.children);
+    } else if (this.props.renderer) {
+      return this.props.renderer();
+    }
+  }
+});
+
+module.exports = DataPreloader;

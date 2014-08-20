8286385be3c14cda44d465a2bf516b7b3cb168cf
Andrey Popp <8mayday@gmail.com>
2014-08-20 14:05:45 +0400

    TextInput: amortize value state changes

--- src/rex.widget/static/js/lib/TextInput.js
+++ src/rex.widget/static/js/lib/TextInput.js
@@ -3,12 +3,74 @@
  */
 'use strict';
 
-var React = require('react/addons');
+var React         = require('react/addons');
+var PropTypes     = React.PropTypes;
+var emptyFunction = require('./emptyFunction');
+
+/**
+ * Mixin which implements `onChange(e)` callback which calls:
+ *
+ *   - `onChangeImmediate(e)` right at the time of `onChange` event is being
+ *     fired.
+ *
+ *   - `onChangeAmortized(e)` amortizing it by `amortizationTimeout` (specified
+ *     via props) milliseconds.
+ *
+ * NOTE: If this functionality can be useful for other widgets consider
+ * implementing it as a decorator component:
+ *
+ *   <AmortizedOnChange onChange={...}>
+ *     <SomeWidgetWithOnChangeEvent />
+ *   </AmortizedOnChange>
+ *
+ * And moving into a separate module.
+ */
+var AmortizedOnChange = {
+
+  propTypes: {
+    amortizationTimeout: PropTypes.number
+  },
+
+  getDefaultProps() {
+    return {
+      amortizationTimeout: 500
+    };
+  },
+
+  componentWillMount() {
+    this._amortizedOnChangeTimer = undefined;
+  },
+
+  componentWillUnmount() {
+    if (this._amortizedOnChangeTimer) {
+      clearTimeout(this._amortizedOnChangeTimer);
+      this._amortizedOnChangeTimer = undefined;
+    }
+  },
+
+  onChange(e) {
+    if (this.onChangeImmediate) {
+      this.onChangeImmediate(e);
+    }
+    if (this._amortizedOnChangeTimer) {
+      clearTimeout(this._amortizedOnChangeTimer);
+    }
+    e.persist();
+    this._amortizedOnChangeTimer = setTimeout(this.onChangeAmortized.bind(null, e), this.props.amortizationTimeout);
+  }
+};
 
 var TextInput = React.createClass({
+  mixins: [AmortizedOnChange],
 
-  render: function() {
-    var value = this.props.value === null ? '' : this.props.value;
+  propTypes: {
+    value: PropTypes.string,
+    onValue: PropTypes.func,
+    placeholder: PropTypes.string
+  },
+
+  render() {
+    var value = this.getValue();
     return this.transferPropsTo(
       <input
         className="rex-widget-TextInput"
@@ -19,9 +81,43 @@ var TextInput = React.createClass({
     );
   },
 
-  onChange: function(e) {
+  getDefaultProps() {
+    return {
+      onValue: emptyFunction,
+      value: null,
+      placeholder: undefined
+    };
+  },
+
+  getInitialState() {
+    return this.getStateFromProps(this.props);
+  },
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(this.getStateFromProps(nextProps));
+  },
+
+  getStateFromProps({value}) {
+    return {value};
+  },
+
+  getValue() {
+    if (this.state.value !== null) {
+      return this.state.value;
+    }
+    return '';
+  },
+
+  onChangeImmediate(e) {
     var value = e.target.value === '' ? null : e.target.value;
-    this.props.onValue(value);
+    this.setState({value});
+  },
+
+  onChangeAmortized(e) {
+    var value = e.target.value === '' ? null : e.target.value;
+    if (this.props.value !== value) {
+      this.props.onValue(value);
+    }
   }
 
 });

5dd59c2ef8c8a48720c38b996a464ce48c19953c
Andrey Popp <8mayday@gmail.com>
2014-08-29 17:45:51 +0400

    0.1.3

--- src/rex.widget/setup.py
+++ src/rex.widget/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.widget',
-    version="0.1.2",
+    version="0.1.3",
     description="Widget toolkit for the RexDB platform",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

e6a390f86a33b1b9fff4d500c1b5b181c582ec08
Andrey Popp <8mayday@gmail.com>
2014-09-04 14:46:50 +0400

    demo: fix config

--- src/rex.widget/demo/rex.widget_demo/static/js/webpack.config.js
+++ src/rex.widget/demo/rex.widget_demo/static/js/webpack.config.js
@@ -1,6 +1,14 @@
 var path = require('path');
 var configureWebpack = require('rex-setup').configureWebpack;
 
+var global_modules = path.join(
+  process.env.NPM_CONFIG_PREFIX,
+  'lib/bower_components');
+
+function global(p) {
+  return path.join(global_modules, p);
+}
+
 module.exports = configureWebpack({
   entry: [
     // FIXME: This is kinda a boilerplate we require at the moment, but it would
@@ -8,5 +16,13 @@ module.exports = configureWebpack({
     path.join(__dirname, 'style/index.less'),
     'rex-setup/introspection/loader?all!rex-widget-demo',
     'rex-setup/introspection/loader?all!rex-widget'
-  ]
+  ],
+
+  resolve: {
+    alias: {
+      // this is because we are using npm dist of React
+      'react': global('react'),
+      'react/addons': global('react/addons')
+    }
+  }
 });

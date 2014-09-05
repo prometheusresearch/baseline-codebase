4fafa11da8de100476d567189c31bfabc6f04b28
Andrey Popp <8mayday@gmail.com>
2014-09-05 15:46:12 +0400

    util: gardening

--- src/rex.widget/src/rex/widget/util.py
+++ src/rex.widget/src/rex/widget/util.py
@@ -11,10 +11,23 @@ import time
 import contextlib
 from .logging import getLogger
 
+
 log = getLogger(__name__)
+
 
 @contextlib.contextmanager
 def measure_execution_time(message='execution time: %f seconds', log=log):
+    """ Measure and log execution time.
+
+    Example::
+
+        with measure_execution_time():
+            potentially_expensive_computation()
+
+
+    :keyword message: Optional message template
+    :keyword log: Optional logger
+    """
     start = time.clock()
     yield
     end = time.clock()

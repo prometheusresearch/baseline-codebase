84064db52887236be75a3f2c34e3bda5ea062e30
Andrey Popp <8mayday@gmail.com>
2014-09-17 17:33:35 +0600

    Autogenerated widget doc (WIP)

---
+++ src/rex.widget/doc/library.rst
@@ -0,0 +1,30 @@
+Widget library
+==============
+
+.. autowidget:: rex.widget.library.Container
+.. autowidget:: rex.widget.library.Block
+.. autowidget:: rex.widget.library.ResizeableBlock
+.. autowidget:: rex.widget.library.CollapsibleBlock
+.. autowidget:: rex.widget.library.LabelWidget
+.. autowidget:: rex.widget.library.HeaderWidget
+.. autowidget:: rex.widget.library.TextWidget
+.. autowidget:: rex.widget.library.UnsafeHTML
+.. autowidget:: rex.widget.library.SectionWidget
+.. autowidget:: rex.widget.library.LinkWidget
+.. autowidget:: rex.widget.library.Panel
+.. autowidget:: rex.widget.library.Tabs
+.. autowidget:: rex.widget.library.Tab
+.. autowidget:: rex.widget.library.RadioButtonGroup
+.. autowidget:: rex.widget.library.CheckboxGroup
+.. autowidget:: rex.widget.library.List
+.. autowidget:: rex.widget.library.TableWidget
+.. autowidget:: rex.widget.library.TwoColumnLayoutWidget
+.. autowidget:: rex.widget.library.SelectWidget
+.. autowidget:: rex.widget.library.TextInputWidget
+.. autowidget:: rex.widget.library.CheckboxWidget
+.. autowidget:: rex.widget.library.FilterWidget
+.. autowidget:: rex.widget.library.FiltersWidget
+.. autowidget:: rex.widget.library.GridWidget
+.. autowidget:: rex.widget.library.ButtonWidget
+.. autowidget:: rex.widget.library.Autocomplete
+.. autowidget:: rex.widget.library.Modal
---
+++ src/rex.widget/src/rex/widget/sphinx/autowidget.py
@@ -0,0 +1,93 @@
+"""
+
+    rex.widget.sphinx.autowidget
+    ============================
+
+    :copyright: 2014, Prometheus Research, LLC
+
+"""
+
+import sphinx.ext.autodoc
+import sphinx.domains.python
+import sphinx.roles
+from sphinx.locale import l_
+
+from rex.widget import Field
+
+
+class WidgetDocumenter(sphinx.ext.autodoc.ClassDocumenter):
+
+    objtype = 'widget'
+
+    # Since these have very specific tests, we give the classes defined here
+    # very high priority so that they override any other documenters.
+    priority = 100 + sphinx.ext.autodoc.ClassDocumenter.priority
+
+    @classmethod
+    def can_document_member(cls, member, membername, isattr, parent):
+        return isinstance(member, Field)
+
+    def format_args(self):
+        return ""
+
+    def filter_members(self, members, want_all):
+        return [
+            (membername, member, False)       
+            for (membername, member) in members
+            if membername != 'states'
+        ]
+
+    def get_object_members(self, want_all):
+        # TODO: check self.options.members and fitler requires fields
+        return False, self.object.fields.items()
+
+
+class WidgetFieldDocumenter(sphinx.ext.autodoc.AttributeDocumenter):
+
+    objtype = 'widgetattribute'
+    directivetype = 'attribute'
+    priority = 100 + sphinx.ext.autodoc.AttributeDocumenter.priority
+
+    @classmethod
+    def can_document_member(cls, member, membername, isattr, parent):
+        return isinstance(member, Field)
+
+    def add_content(self, more_content, no_docstring=False):
+        # Revert back to default since the docstring *is* the correct thing to
+        # display here.
+        sphinx.ext.autodoc.ClassLevelDocumenter.add_content(
+            self, more_content, no_docstring)
+        self.render_validator()
+        self.render_default_value()
+
+    def render_validator(self):
+        sourcename = u'docstring of %s' % self.fullname
+        self.add_line('* **Validator**: ``%r``' % self.object.validator, sourcename)
+
+    def render_default_value(self):
+        sourcename = u'docstring of %s' % self.fullname
+        if self.object.default is not NotImplemented:
+            self.add_line('* **Default value**: ``%r``' % self.object.default, sourcename)
+        else:
+            self.add_line('* **field is required**', sourcename)
+
+class WidgetDirective(sphinx.domains.python.PyClasslike):
+
+    def get_index_text(self, modname, name_cls):
+        if self.objtype == 'widget':
+            if not modname:
+                return '%s (built-in widget)' % name_cls[0]
+            return '%s (%s widget)' % (name_cls[0], modname)
+        else:
+            return ''
+
+
+def setup(app):
+    app.add_autodocumenter(WidgetDocumenter)
+    app.add_autodocumenter(WidgetFieldDocumenter)
+
+    domain = sphinx.domains.python.PythonDomain
+    domain.object_types['widget'] = sphinx.domains.python.ObjType(
+        l_('widget'), 'widget', 'obj')
+    domain.directives['widget'] = WidgetDirective
+    domain.roles['widget'] = sphinx.domains.python.PyXRefRole()

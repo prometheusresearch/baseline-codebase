a896e6a8ed603560615343f7420e6e710f1645be
Andrey Popp <8mayday@gmail.com>
2014-10-30 14:36:30 +0800

    Merge with upstream.

--- src/rex.widget/setup.py
+++ src/rex.widget/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.widget',
-    version="0.2.4-dev",
+    version="0.2.4",
     description="Widget toolkit for the RexDB platform",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

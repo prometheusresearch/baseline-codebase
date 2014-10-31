4822814df03bcd6dc719bc999d2ab3ca0c52ef85
Dmitry Druganov <drud@prometheusresearch.com>
2014-10-31 14:20:47 +0100

    fixed version

--- src/rex.formbuilder/setup.py
+++ src/rex.formbuilder/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.formbuilder',
-    version='3.0.1',
+    version='3.0.2',
     description='The RexFormbuilder application, a tool for creating and'
     'managing the Instruments and Forms in the RexDB platform',
     long_description=open('README.rst', 'r').read(),

072510ce0d6bc3899c2a909e3123975cc8b63814
Jason Simeone <jason@prometheusresearch.com>
2015-01-15 10:57:57 -0500

    Support for rex.setup v2

--- src/rex.sms/NEWS.rst
+++ src/rex.sms/NEWS.rst
@@ -5,6 +5,12 @@ REX.SMS Change Log
 .. contents:: Table of Contents
 
 
+0.2.0 (2015-01-xx)
+==================
+
+* Added support for ``rex.setup`` v2.
+
+
 0.1.0 (2014-12-16)
 ==================
 
--- src/rex.sms/setup.py
+++ src/rex.sms/setup.py
@@ -8,19 +8,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.sms',
-    version='0.1.0',
-    description='',
+    version='0.2.0',
+    description='A RexDB interface for sending SMS text messages.',
     long_description=open('README.rst', 'r').read(),
-    maintainer='Prometheus Research, LLC',
-    maintainer_email='contact@prometheusresearch.com',
+    author='Prometheus Research, LLC',
+    author_email='contact@prometheusresearch.com',
     license='AGPLv3',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ],
     url='https://bitbucket.org/rexdb/rex.sms',
     package_dir={'': 'src'},
     packages=find_packages('src'),
     include_package_data=True,
     namespace_packages=['rex'],
     setup_requires=[
-        'rex.setup>=1.0,<2',
+        'rex.setup>=1,<3',
     ],
     install_requires=[
         'rex.core>=1.1,<2',

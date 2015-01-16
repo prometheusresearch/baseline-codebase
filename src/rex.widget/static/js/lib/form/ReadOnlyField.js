85282f2e33fd5d437475b4af4b900b83f7bca773
Andrey Popp <8mayday@gmail.com>
2015-01-16 17:58:19 +0800

    Style fixes to <ReadOnlyField />.

--- src/rex.widget/static/js/lib/form/ReadOnlyField.js
+++ src/rex.widget/static/js/lib/form/ReadOnlyField.js
@@ -6,10 +6,19 @@ var React             = require('react/addons');
 var cx                = React.addons.classSet;
 var ReactForms        = require('react-forms');
 var {Box, HBox}       = require('../layout');
+var merge             = require('../merge');
 var FormContextMixin  = require('./FormContextMixin');
 
 var ReadOnlyField = React.createClass({
   mixins: [FormContextMixin],
+
+  compactLabelStyle: merge(
+    Box.makeBoxStyle(),
+    {
+      textAlign: 'right',
+      marginRight: 10
+    }
+  ),
 
   render() {
     var {compact, className, label, hint, ...props} = this.props;
@@ -19,6 +28,7 @@ var ReadOnlyField = React.createClass({
         <HBox {...props} className={cx('rw-ReadOnlyField', className)}>
           <Box size={1}>
             <ReactForms.Label
+              style={this.compactLabelStyle}
               label={label}
               hint={hint}
               />

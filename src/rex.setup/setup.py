68e4d57bd402da9b176137836befb02dc767a666
Andrey Popp <8mayday@gmail.com>
2015-01-20 00:00:40 +0800

    Bump version to 2.0.1

--- src/rex.setup/setup.py
+++ src/rex.setup/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.setup',
-    version = "2.0.0",
+    version = "2.0.1",
     description="Distutils extension for the RexDB platform",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

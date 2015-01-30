8e2a924cb9d45117dbafa0218e397eb03346933a
Kirill Simonov <xi@resolvent.net>
2015-01-30 13:13:40 -0600

    Updated release date.

--- src/rex.port/NEWS.rst
+++ src/rex.port/NEWS.rst
@@ -5,7 +5,7 @@
 .. contents:: Table of Contents
 
 
-1.0.3 (2015-XX-XX)
+1.0.3 (2015-01-30)
 ==================
 
 * Updated test output.
--- src/rex.port/demo/setup.py
+++ src/rex.port/demo/setup.py
@@ -32,7 +32,7 @@ class demo(Command):
 
 setup(
     name='rex.port_demo',
-    version = "1.0.2",
+    version = "1.0.3",
     description="Demo package for testing rex.port",
     setup_requires=[
         'rex.setup',

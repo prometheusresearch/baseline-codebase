bdf6af805ca8d9f127de95ee1d6d9bf56cbb06b8
Drew McCabe <drew@prometheusresearch.com>
2015-02-16 20:47:57 +0000

    bumping rex.i18n dependency for update to 0.4.1

--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -33,7 +33,7 @@ setup(
         'rex.deploy==2.2.0',
         'rex.expression==1.5.0',
         'rex.forms==0.28.0',
-        'rex.i18n==0.4.0',
+        'rex.i18n==0.4.1',
         'rex.instrument==0.14.0',
         'rex.port==1.0.3',
         'rex.restful==0.3.0',

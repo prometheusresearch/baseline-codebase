b127739fa21d6b70a2346e8ca1454ec85f3ce64e
Andrey Popp <8mayday@gmail.com>
2015-02-18 23:17:49 +0800

    Merge with upstream

--- src/rex.widget/static/js/lib/form/FileDownloadField.js
+++ src/rex.widget/static/js/lib/form/FileDownloadField.js
@@ -3,25 +3,241 @@
  */
 'use strict';
 
+var Promise           = require('bluebird');
 var React             = require('react/addons');
+var cx                = React.addons.classSet;
+var merge             = require('../merge');
+var {Box, HBox}       = require('../layout');
+var Button            = require('../Button');
+var Icon              = require('../Icon');
+var Hoverable         = require('../Hoverable');
+var FieldBase         = require('./FieldBase');
 var FormContextMixin  = require('./FormContextMixin');
-var {StoredFile}      = require('./File');
 
-var FileDownloadField = React.createClass({
-  mixins: [FormContextMixin],
+function uploadFile(url, file, onProgress) {
+  return new Promise(function(resolve, reject) {
+    var data = new FormData();
+    data.append('file', file);
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url, true);
+    xhr.onload = resolve;
+    xhr.onerror = reject;
+    if (onProgress) {
+      xhr.upload.onprogress = function(e) {
+        if (e.lengthComputable) {
+          var progress = e.loaded / e.total;
+          onProgress(progress);
+        }
+      };
+    }
+    try {
+      xhr.send(data);
+    } catch(err) {
+      reject(err);
+    }
+  });
+}
+
+function unquote(str) {
+  return str.replace(/^'/, '').replace(/'$/, '');
+}
+
+function filename(file) {
+  var result = unquote(file.name);
+  return result.replace(/.*\/([^\/]+)$/i, '$1');
+}
+
+var Progress = React.createClass({
+
+  style: {
+    height: 2,
+    background: 'rgb(142, 142, 226)'
+  },
 
   render() {
-    var {download, ...props} = this.props;
-    var value = this.getValue();
-    if (!value) {
-      return null;
+    var {progress, style, ...props} = this.props;
+    progress = progress || 0;
+    var style = merge(this.style, style, {
+      width: `${progress * 100}%`
+    });
+    return <Box {...props} style={style} />;
+  }
+});
+
+var File = React.createClass({
+  mixins: [Hoverable],
+
+  style: {
+    fontSize: '90%',
+    cursor: 'pointer',
+    top: 2
+  },
+
+  styleIcon: {
+    marginRight: 5,
+    marginLeft: 5,
+    top: -2
+  },
+
+  render() {
+    var {file, icon, required, children, onRemove, progress, ...props} = this.props;
+    var {hover} = this.state;
+    return (
+      <Box {...props}>
+        <HBox
+          {...this.hoverable}
+          size={1}
+          style={this.style}
+          onClick={!required && !progress && onRemove}>
+          <Box centerVertically style={this.styleIcon}>
+            {icon ?
+              <Icon name={icon} /> :
+              progress ?
+              <Icon name="repeat" /> :
+              required || !hover ?
+              <Icon name="ok" /> :
+              <Icon name="remove" />}
+          </Box>
+          <Box centerVertically>
+            {progress || required || !hover ?
+              children || file.name :
+              'Remove file'}
+          </Box>
+        </HBox>
+        <Progress progress={progress} />
+      </Box>
+    );
+  }
+});
+
+var StoredFile = React.createClass({
+
+  render() {
+    var {download, file, ownerRecordID, ...props} = this.props;
+    return (
+      <File icon="download" required {...props} file={file}>
+        <a href={`${download}?${ownerRecordID}`}>
+          {filename(file)}
+        </a>
+      </File>
+    );
+  }
+});
+
+var FileUploadInput = React.createClass({
+
+  styleInput: {
+    display: 'none'
+  },
+
+  stylePlaceholder: {
+    fontSize: '90%',
+    color: '#AAAAAA'
+  },
+
+  styleError: {
+    fontSize: '90%',
+    color: 'rgb(234, 69, 69)'
+  },
+
+  render() {
+    var {value, required, download, ownerRecordID, ...props} = this.props;
+    var {file, progress, error} = this.state;
+    // the case when we need to render a file stored in storage
+    var renderStoredFile = !file && value;
+    if (renderStoredFile) {
+      file = {name: value};
     }
     return (
-      <StoredFile
-        {...props}
-        file={{name: value}}
-        download={download}
+      <HBox {...props} onChange={undefined} storage={undefined}>
+        {error ?
+          <Box centerVertically margin="0 0 0 10px" size={1}>
+            <Box style={this.styleError}>{error.message}</Box>
+          </Box> :
+          renderStoredFile ?
+          <StoredFile
+            file={file}
+            download={download}
+            ownerRecordID={ownerRecordID}
+            /> :
+          file ?
+          <File
+            size={1}
+            margin="0 0 0 10px"
+            required={required}
+            progress={progress}
+            file={file}
+            onRemove={this.onRemove}
+            /> :
+          <Box centerVertically margin="0 0 0 10px" size={1}>
+            <Box style={this.stylePlaceholder}>No file choosen</Box>
+          </Box>}
+      </HBox>
+    );
+  },
+
+  getInitialState() {
+    return {
+      file: null,
+      progress: null,
+      error: null
+    };
+  },
+
+  onChange(e) {
+    var files = e.target.files;
+    var file = files[0];
+    this.setState({
+      file,
+      error: null
+    });
+    uploadFile(this.props.storage, file, this._onUploadProgress)
+      .then(response => JSON.parse(response.target.responseText))
+      .then(this._onUploadComplete, this._onUploadError);
+  },
+
+  onClick() {
+    this.refs.underlying.getDOMNode().click()
+  },
+
+  onRemove() {
+    this.setState(this.getInitialState());
+    this.props.onChange(undefined);
+  },
+
+  _onUploadProgress(progress) {
+    this.setState({progress});
+  },
+
+  _onUploadComplete(response) {
+    this.setState({progress: null});
+    this.props.onChange(response.file);
+  },
+
+  _onUploadError(error) {
+    this.setState({progress: null, error});
+  }
+});
+
+var FileDownloadField = React.createClass({
+  contextTypes: FormContextMixin.contextTypes,
+
+  render() {
+    var {className, storage, download, required, value, ...props} = this.props;
+    var input = (
+      <FileUploadInput
         ownerRecordID={this._getOwnerRecordID()}
+        storage={storage}
+        download={download}
+        />
+    );
+    return (
+      <FieldBase
+        {...props}
+        className={cx('rw-FileDownloadField', className)}
+        input={input}
+        value={value}
+        key={value.value.get('id')}
         />
     );
   },
@@ -35,14 +251,5 @@ var FileDownloadField = React.createClass({
     return record ? record.get('id') : undefined;
   }
 });
-
-function filename(file) {
-  var result = unquote(file.name);
-  return result.replace(/.*\/([^\/]+)$/i, '$1');
-}
-
-function unquote(str) {
-  return str.replace(/^'/, '').replace(/'$/, '');
-}
 
 module.exports = FileDownloadField;

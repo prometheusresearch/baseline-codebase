604975f522f7302b60a6f38439c5ee7e6b4c5dfd
Kirill Simonov <xi@resolvent.net>
2015-02-19 14:00:28 -0600

    doc bundle: do not strip leading _.

--- src/rex.setup/src/rex/setup/doc.py
+++ src/rex.setup/src/rex/setup/doc.py
@@ -6,7 +6,6 @@
 from .generate import Generate
 from .commonjs import exe
 import os, os.path
-import subprocess
 import distutils.log, distutils.errors
 
 
@@ -30,25 +29,9 @@ class GenerateDoc(Generate):
         from sphinx.application import Sphinx
         sphinx = Sphinx(source, source, self.target, self.target, builder,
                         status=None)
-        sphinx.connect('html-page-context', self.on_context)
-        sphinx.connect('build-finished', self.on_finished)
         sphinx.build()
-
-    def on_context(self, app, pagename, templatename, context, doctree):
-        # Replaces `pathto` implementation.
-        _pathto = context.get('pathto')
-        def pathto(uri, *args, **kwds):
-            return _pathto(uri.lstrip('_'), *args, **kwds)
-        context['pathto'] = pathto
-
-    def on_finished(self, app, exc):
-        # Renames `_<name>` to `<name>`; builds PDF.
-        for name in ['sources', 'static', 'images']:
-            src = os.path.join(app.builder.outdir, '_'+name)
-            dst = os.path.join(app.builder.outdir, name)
-            if os.path.exists(src):
-                os.rename(src, dst)
-        if app.builder.name == 'latex':
-            exe('make', ['all-pdf'], cwd=app.builder.outdir, commonjs=False)
+        # Build PDF.
+        if builder == 'latex':
+            exe('make', ['all-pdf'], cwd=self.target, commonjs=False)
 
 

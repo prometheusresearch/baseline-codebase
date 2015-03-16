2cddebcf776214bb73c1161b4fc769b8e921d00b
Jason Simeone <jason@prometheusresearch.com>
2015-03-16 08:42:53 -0400

    Made the dep version ranges safer.

--- src/rex.formbuilder/setup.py
+++ src/rex.formbuilder/setup.py
@@ -29,7 +29,7 @@ setup(
     install_requires=[
         'rex.core>=1.10,<2',
         'rex.web>=3.2,<4',
-        'rex.forms>=0.28.1,<2',
+        'rex.forms>=0.28.1,<0.29',
         'rex.restful>=0.3,<0.4',
         'rex.widget>=0.2,<0.3',
         'rex.i18n>=0.4,<0.5',

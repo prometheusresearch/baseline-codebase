46414cf497c7ae2b13ef018a390955a262b7b84e
Andrey Popp <8mayday@gmail.com>
2015-03-17 01:11:28 +0800

    Add TimeoutMixin

---
+++ src/rex.widget/static/js/lib/modern/TimeoutMixin.js
@@ -0,0 +1,35 @@
+/**
+ * @copyright 2015, Prometheus Research, LLC
+ */
+'use strict';
+
+var TimeoutMixin = {
+
+  componentWillMount() {
+    this._timeouts = [];
+  },
+
+  componentWillUnmount() {
+    this._timeouts.forEach(this.clearTimeout);
+    this._timeouts = null;
+  },
+
+  setTimeout(func, ms) {
+    var timeoutID = setTimeout(() => {
+      this.clearTimeout(timeoutID);
+      func();
+    }, ms);
+    this._timeouts.push(timeoutID);
+    return timeoutID;
+  },
+
+  clearTimeout(timeoutID) {
+    var idx = this._timeouts.indexOf(timeoutID);
+    if (idx > -1) {
+      this._timeouts.splice(idx, 1);
+    }
+  }
+
+};
+
+module.exports = TimeoutMixin;

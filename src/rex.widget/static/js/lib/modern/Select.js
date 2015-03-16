6f4abdf959ee3f2ab6b15816b735159fa8ad6882
Andrey Popp <8mayday@gmail.com>
2015-03-17 00:18:44 +0800

    modern/Select: wrap in Box

--- src/rex.widget/static/js/lib/modern/Select.js
+++ src/rex.widget/static/js/lib/modern/Select.js
@@ -3,4 +3,25 @@
  */
 'use strict';
 
-module.exports = require('../Select');
+var React       = require('react');
+var BaseSelect  = require('../Select');
+var {VBox}      = require('./Layout');
+
+var Select = React.createClass({
+  
+  render() {
+    var {noEmptyValue, data, value, onChange, ...props} = this.props;
+    return (
+      <VBox {...props}>
+        <BaseSelect
+          noEmptyValue={noEmptyValue}
+          data={data}
+          value={value}
+          onValue={onChange}
+          />
+      </VBox>
+    );
+  }
+});
+
+module.exports = Select;

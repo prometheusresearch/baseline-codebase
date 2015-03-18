9a5a553cc5522a90daa165d635f074cba571e4b8
Kirill Simonov <xi@resolvent.net>
2015-03-18 18:09:16 -0500

    Another attempt to fix a problem with namespace packages.

--- src/rex.setup/src/rex/setup/namespace.py
+++ src/rex.setup/src/rex/setup/namespace.py
@@ -15,21 +15,32 @@ class develop_namespace(setuptools.Command):
     description = "fix lookup issues with namespace packages"
 
     def initialize_options(self):
-        self.namespace_packages = self.distribution.namespace_packages or []
+        self.namespaces = []
+        for package in self.distribution.namespace_packages or []:
+            package = package.split('.')
+            namespace = ()
+            for name in package:
+                namespace = namespace + (name,)
+                if namespace not in self.namespaces:
+                    self.namespaces.append(namespace)
+        self.namespaces.sort()
 
     def finalize_options(self):
         pass
 
     def run(self):
-        for name in self.namespace_packages:
+        for namespace in self.namespaces:
             develop = self.get_finalized_command("develop")
-            pth_path = os.path.join(develop.install_dir, "nsdev-%s.pth" % name)
-            if os.path.exists(pth_path):
+            package_dir = os.path.join(develop.install_dir, *namespace)
+            init_path = os.path.join(package_dir, '__init__.py')
+            if os.path.exists(init_path):
                 continue
-            distutils.log.info("Creating %s", pth_path)
-            with open(pth_path, 'w') as pth:
-                pth.write("import pkg_resources;"
-                          " pkg_resources.declare_namespace(%r)\n" % name)
+            distutils.log.info("Creating %s", init_path)
+            if not os.path.exists(package_dir):
+                os.mkdir(package_dir)
+            with open(init_path, 'w') as init:
+                init.write("__import__('pkg_resources')"
+                           ".declare_namespace(__name__)\n")
 
 
 # Patch `develop` command to call `develop_namespace`.

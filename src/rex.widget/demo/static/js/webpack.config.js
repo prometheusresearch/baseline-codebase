b9e495e0ba1163e95d69f99a7f54074b86ef5fcd
Andrey Popp <8mayday@gmail.com>
2015-03-19 23:31:33 +0800

    Merge with upstream

--- src/rex.widget/demo/static/js/webpack.config.js
+++ src/rex.widget/demo/static/js/webpack.config.js
@@ -8,11 +8,17 @@ module.exports = RexSetup.configureWebpack({
     path.join(__dirname, 'style/index.less'),
     'rex-setup/introspection/loader?all!rex-widget-demo',
     'rex-setup/introspection/loader?all!rex-widget',
-    //'rex-setup/introspection/loader?all!rex-charts'
   ],
 
   resolve: {
       alias: {
+        'util': require.resolve('webpack/node_modules/node-libs-browser/node_modules/util/util.js'),
+        'console': require.resolve('webpack/node_modules/node-libs-browser/node_modules/console-browserify'),
+        'date-now': require.resolve('webpack/node_modules/node-libs-browser/node_modules/console-browserify/node_modules/date-now'),
+        'inherits': require.resolve('webpack/node_modules/node-libs-browser/node_modules/util/node_modules/inherits/inherits_browser.js'),
+        'is-array': require.resolve('webpack/node_modules/node-libs-browser/node_modules/buffer/node_modules/is-array'),
+        'ieee754': require.resolve('webpack/node_modules/node-libs-browser/node_modules/buffer/node_modules/ieee754'),
+        'base64-js': require.resolve('webpack/node_modules/node-libs-browser/node_modules/buffer/node_modules/base64-js'),
         'react/addons': RexSetup.packagePath('react/addons.js'),
         'react': RexSetup.packagePath('react/addons.js')
       }

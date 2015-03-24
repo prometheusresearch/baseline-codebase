bb56ba14f83869eb7ebe4e04a44577d717ab5d93
Jason Simeone <jason@prometheusresearch.com>
2015-03-24 08:57:56 -0400

    Fixed demo version.

--- src/rex.forms/demo/setup.py
+++ src/rex.forms/demo/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.forms_demo',
-    version='0.28.0',
+    version='0.28.1',
     description='Demo package for testing rex.forms',
     package_dir={'': 'src'},
     packages=find_packages('src'),

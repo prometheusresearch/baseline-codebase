3a6f161881f690e7b0f4eec40d9845a8ea599fc6
Andrey Popp <8mayday@gmail.com>
2015-03-27 23:34:36 +0800

    Bump version to 4.3.1

--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='rex.platform',
-    version='4.3.0',
+    version='4.3.1',
     description='RexDB Capstone Project',
     long_description=open('README.rst', 'r').read(),
     author='Prometheus Research, LLC',

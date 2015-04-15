9ea494116965c3042d2cc0edab27e37aefc72df2
Oleksiy Golovko <zindel@prometheusresearch.com>
2015-04-15 13:04:21 +0200

    update rex.widget dependency and bump version

--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='rex.platform',
-    version='4.3.5',
+    version='4.3.6',
     description='RexDB Capstone Project',
     long_description=open('README.rst', 'r').read(),
     author='Prometheus Research, LLC',
@@ -37,7 +37,7 @@ setup(
         'rex.restful==0.4.0',
         'rex.urlmap==2.6.1',
         'rex.web==3.3.1',
-        'rex.widget==0.2.19',
+        'rex.widget==0.2.20',
     ],
 )
 

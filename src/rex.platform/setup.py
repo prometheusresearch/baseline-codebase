8d61495f1e8cfb73f12787d77cd7a31e923e1ec4
Oleksiy Golovko <zindel@prometheusresearch.com>
2015-04-17 14:56:49 +0200

    update rex.web dependency

--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -36,7 +36,7 @@ setup(
         'rex.port==1.0.4',
         'rex.restful==0.4.0',
         'rex.urlmap==2.6.1',
-        'rex.web==3.3.1',
+        'rex.web==3.4.0',
         'rex.widget==0.2.20',
     ],
 )

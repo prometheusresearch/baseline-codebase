e961bd9b28fba16deaf496be4ca6c054816fe4ab
Andrey Popp <8mayday@gmail.com>
2015-05-27 12:34:39 +0300

    Make wizard apps operate without bundle.

--- src/rex.action/demo/setup.py
+++ src/rex.action/demo/setup.py
@@ -4,18 +4,10 @@ setup(
     name='rex.wizard_demo',
     version = "0.1.0",
     description="Demo package for testing rex.wizard",
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
-    namespace_packages=['rex'],
     install_requires=[
-        'rex.wizard'
+        'rex.wizard',
+        'rex.deploy',
     ],
-    rex_init='rex.wizard_demo',
-    rex_static='static',
-    rex_bundle={
-        './www/bundle': [
-            'webpack:rex-wizard-demo'
-        ]
-    },
+    rex_static='static'
 )
 

882029a406b613aa88d4e530313497e3bd378929
Andrey Popp <8mayday@gmail.com>
2015-06-11 09:51:33 +0200

    <NumberField />: fix to formatting

--- src/rex.widget/static/js/lib/forms/NumberField.js
+++ src/rex.widget/static/js/lib/forms/NumberField.js
@@ -7,21 +7,51 @@ var React = require('react/addons');
 var Field = require('./Field');
 var Input = require('./Input');
 
+var NUMBER_RE = /^\-?[0-9]+(\.[0-9]*)?$/;
+
 var NumberInput = React.createClass({
 
   render() {
     return (
       <Input
         {...this.props}
+        value={this.state.value}
         onChange={this.onChange}
         />
     );
   },
 
+  getInitialState() {
+    return {
+      value: this.props.value
+    };
+  },
+
+  componentWillReceiveProps(nextProps) {
+    var value = this._renderedComponent._pendingState ?
+      this._renderedComponent._pendingState.value :
+      this.state.value;
+    if (nextProps.value === undefined) {
+      this.setState({value: ''});
+    } else if (nextProps.value !== parseFloat(value, 10)) {
+      this.setState({value: String(nextProps.value)});
+    }
+  },
+
   onChange(e) {
+    var prevValue = this.props.value;
     var value = e.target.value;
-    value = parseFloat(value, 10);
-    this.props.onChange(value);
+    this.setState({value});
+    if (value === '') {
+      this.props.onChange(undefined);
+    } else {
+      var parsed = parseFloat(value, 10);
+      if (isNaN(parsed) || !NUMBER_RE.exec(value)) {
+        this.props.onChange(value);
+      } else {
+        this.props.onChange(parsed);
+      }
+    }
   }
 
 });

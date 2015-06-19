b142d24b32616408664b98ee0cf2d1c7b40bec88
Andrey Popp <8mayday@gmail.com>
2015-06-19 11:48:02 +0200

    StyleUtils: refactor to be a set of functions/constants.

--- src/rex.widget/static/js/lib/StyleUtils.js
+++ src/rex.widget/static/js/lib/StyleUtils.js
@@ -5,71 +5,71 @@
 
 var isString = require('./isString');
 
-var StyleUtils = {
+function boxShadow(offsetX, offsetY, blurRadius, spreadRadius, color) {
+  return `${offsetX}px ${offsetY}px ${blurRadius}px ${spreadRadius}px ${color}`;
+}
 
-  boxShadow(offsetX, offsetY, blurRadius, spreadRadius, color) {
-    return `${offsetX}px ${offsetY}px ${blurRadius}px ${spreadRadius}px ${color}`;
-  },
+function insetBoxShadow(offsetX, offsetY, blurRadius, spreadRadius, color) {
+  return `inset ${boxShadow(offsetX, offsetY, blurRadius, spreadRadius, color)}`;
+}
 
-  insetBoxShadow(offsetX, offsetY, blurRadius, spreadRadius, color) {
-    return `inset ${StyleUtils.boxShadow(offsetX, offsetY, blurRadius, spreadRadius, color)}`;
-  },
-
-  rgba(r, g, b, a) {
-    if (b === undefined && a === undefined) {
-      a = g;
-      g = r;
-      b = r;
-    }
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
-  },
-
-  rgb(r, g, b) {
-    if (g === undefined && b === undefined) {
-      g = r;
-      b = r;
-    }
-    return `rgb(${r}, ${g}, ${b})`;
-  },
-
-  border(width, style, color) {
-    return `${width}px ${style} ${color}`;
-  },
-
-  borderStyle: {
-    solid: 'solid'
-  },
-
-  linearGradient(direction, ...colorStops) {
-    colorStops = colorStops
-      .map(p => isString(p) ? p : `${p.color} ${p.value}`)
-      .join(', ');
-    return `linear-gradient(${direction}, ${colorStops})`;
-  },
-
-  transform(duration) {
-    return `transform ${duration}s`;
-  },
-
-  translate3d(x, y, z) {
-    return `translate3d(${x}px, ${y}px, ${z}px)`;
-  },
-
-  position: {
-    absolute: 'absolute',
-    relative: 'relative',
-    fixed: 'fixed'
-  },
-
-  cursor: {
-    pointer: 'pointer'
-  },
-
-  overflow: {
-    auto: 'auto',
-    hidden: 'hidden',
-    scroll: 'scroll'
+function rgba(r, g, b, a) {
+  if (b === undefined && a === undefined) {
+    a = g;
+    g = r;
+    b = r;
   }
+  return `rgba(${r}, ${g}, ${b}, ${a})`;
+}
+
+function rgb(r, g, b) {
+  if (g === undefined && b === undefined) {
+    g = r;
+    b = r;
+  }
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function border(width, style, color) {
+  return `${width}px ${style} ${color}`;
+}
+
+var borderStyle = {
+  solid: 'solid'
 };
 
-module.exports = StyleUtils;
+function linearGradient(direction, ...colorStops) {
+  colorStops = colorStops
+    .map(p => isString(p) ? p : `${p.color} ${p.value}`)
+    .join(', ');
+  return `linear-gradient(${direction}, ${colorStops})`;
+}
+
+function transform(duration) {
+  return `transform ${duration}s`;
+}
+
+function translate3d(x, y, z) {
+  return `translate3d(${x}px, ${y}px, ${z}px)`;
+}
+
+var position = {
+  absolute: 'absolute',
+  relative: 'relative',
+  fixed: 'fixed'
+};
+
+var cursor = {
+  pointer: 'pointer'
+};
+
+var overflow = {
+  auto: 'auto',
+  hidden: 'hidden',
+  scroll: 'scroll'
+};
+
+module.exports = {
+  overflow, cursor, position, translate3d, transform, linearGradient, borderStyle,
+  border, rgb, rgba, insetBoxShadow, boxShadow
+};

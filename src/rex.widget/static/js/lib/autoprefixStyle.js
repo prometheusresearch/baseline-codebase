cafd5efe485bbf99873ada3f0bbbf1d080e15ec5
Andrey Popp <8mayday@gmail.com>
2015-06-22 13:26:45 +0200

    autoprefixStyle: do not emit prefixed version when native is supported

--- src/rex.widget/static/js/lib/autoprefixStyle.js
+++ src/rex.widget/static/js/lib/autoprefixStyle.js
@@ -1,8 +1,13 @@
 /**
  * @copyright 2015, Pete Hunt <floydophone@gmail.com>
- * @copyright 2015, Pete Hunt <floydophone@gmail.com>
+ * @copyright 2015, Prometheus Research LLC
  */
 'use strict';
+
+var DIV = document.createElement('div');
+
+var flexboxNative = 'flex' in DIV.style;
+var flexboxWebkit = !flexboxNative && 'WebkitFlex' in DIV.style;
 
 function autoprefixStyle(style) {
   if (style.hasOwnProperty('userSelect')) {
@@ -28,40 +33,40 @@ function autoprefixStyle(style) {
     style.MozOsxFontSmoothing = style.fontSmoothing === 'antialiased' ? 'grayscale' : undefined;
   }
   
-  if (style.hasOwnProperty('flex')) {
+  if (style.hasOwnProperty('flex') && flexboxWebkit) {
     style.WebkitFlex = style.flex;
   }
 
-  if (style.hasOwnProperty('flexDirection')) {
+  if (style.hasOwnProperty('flexDirection') && flexboxWebkit) {
     style.WebkitFlexDirection = style.flexDirection;
   }
 
-  if (style.hasOwnProperty('flexWrap')) {
+  if (style.hasOwnProperty('flexWrap') && flexboxWebkit) {
     style.WebkitFlexWrap = style.flexWrap;
   }
 
-  if (style.hasOwnProperty('alignItems')) {
+  if (style.hasOwnProperty('alignItems') && flexboxWebkit) {
     style.WebkitAlignItems = style.alignItems;
   }
 
-  if (style.hasOwnProperty('flexGrow')) {
+  if (style.hasOwnProperty('flexGrow') && flexboxWebkit) {
     style.WebkitFlexGrow = style.flexGrow;
   }
 
-  if (style.hasOwnProperty('flexShrink')) {
+  if (style.hasOwnProperty('flexShrink') && flexboxWebkit) {
     style.WebkitFlexShrink = style.flexShrink;
   }
 
-  if (style.hasOwnProperty('order')) {
+  if (style.hasOwnProperty('order') && flexboxWebkit) {
     style.WebkitOrder = style.order;
   }
 
-  if (style.hasOwnProperty('justifyContent')) {
+  if (style.hasOwnProperty('justifyContent') && flexboxWebkit) {
     style.WebkitJustifyContent = style.justifyContent;
   }
 
-  if (style.display === 'flex') {
-    style.display = style.display + ';display:-webkit-flex;display:-ms-flexbox';
+  if (style.display === 'flex' && flexboxWebkit) {
+    style.display = style.display + ';display:-webkit-flex;';
   }
 
   return style;

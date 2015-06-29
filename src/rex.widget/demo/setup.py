3fcf8efd25ba6a7989df673aea0828ff8bf7f2f2
Andrey Popp <8mayday@gmail.com>
2015-06-29 11:50:59 +0200

    Bump version

--- src/rex.widget/demo/setup.py
+++ src/rex.widget/demo/setup.py
@@ -22,7 +22,7 @@ class demo(Command):
 
 setup(
     name='rex.widget_demo',
-    version = "1.0.2",
+    version = "1.0.3",
     description="Demo package for testing rex.widget",
     namespace_packages=['rex'],
     package_dir={'': 'src'},

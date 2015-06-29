be0902ada741233e0b866d0ae889fc37ccee9314
Andrey Popp <8mayday@gmail.com>
2015-06-29 11:35:08 +0200

    Fix building with npm 1.3.x.

--- src/rex.action/setup.py
+++ src/rex.action/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.action',
-    version="0.2.0",
+    version="0.2.1",
     description="Foundation of the RexDB platform",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

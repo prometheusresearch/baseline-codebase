4ee30bc935cece45a60a2a70869dcad3e68d7e3f
Oleksiy Golovko <zindel@prometheusresearch.com>
2015-07-31 10:23:16 +0200

    bump version

--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='rex.platform',
-    version='4.7.0',
+    version='4.8.0',
     description='RexDB Capstone Project',
     long_description=open('README.rst', 'r').read(),
     author='Prometheus Research, LLC',

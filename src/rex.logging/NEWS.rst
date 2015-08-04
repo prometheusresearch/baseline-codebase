1f4860f9b9a2138db671b50e85a8e3bbdd7abb3a
Jason Simeone <jason@prometheusresearch.com>
2015-08-04 16:41:15 -0400

    Version bump

--- src/rex.logging/NEWS.rst
+++ src/rex.logging/NEWS.rst
@@ -5,6 +5,12 @@ REX.LOGGING Change Log
 .. contents:: Table of Contents
 
 
+1.0.0 (2015-xx-xx)
+==================
+
+* Major release!
+
+
 0.1.0 (2015-06-19)
 ==================
 
--- src/rex.logging/setup.py
+++ src/rex.logging/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.logging',
-    version='0.1.0',
+    version='1.0.0',
     description='A convenience wrapper to bootstrap the Python logging'
     ' framework via RexDB settings.',
     long_description=open('README.rst', 'r').read(),

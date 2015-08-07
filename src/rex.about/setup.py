0bcf846e319452ebb7a7d7787a1e71f04a51d69c
Andrey Popp <8mayday@gmail.com>
2015-08-07 15:39:49 +0300

    Bump version

--- src/rex.about/setup.py
+++ src/rex.about/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.about',
-    version='0.1.0',
+    version='0.1.1',
     description='A RexDB applet for displaying application versions and'
     ' licensing information',
     long_description=open('README.rst', 'r').read(),

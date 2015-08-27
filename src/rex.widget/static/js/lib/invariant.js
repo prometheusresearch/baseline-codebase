1b530e044ff0633a78f96c29a9785035638ceea3
Andrey Popp <8mayday@gmail.com>
2015-08-27 11:04:51 +0300

    Use `invariant` package for invariant(cond, message) function

--- src/rex.widget/static/js/lib/invariant.js
+++ src/rex.widget/static/js/lib/invariant.js
@@ -1,21 +1,5 @@
 /**
- * @jsx React.DOM
+ * @copyright 2015, Prometheus Research, LLC
  */
-'use strict';
 
-function invariant(cond, msg, a, b, c, d, e, f) {
-  if (!cond) {
-    if (msg) {
-      var replacements = [a, b, c, d, e, f];
-      var idx = 0;
-      msg = msg.replace(/%s/g, function() {
-        return replacements[idx++];
-      });
-      throw new Error('Invariant violation: ' + msg);
-    } else {
-      throw new Error('Invariant violation');
-    }
-  }
-}
-
-module.exports = invariant;
+module.exports = require('invariant');

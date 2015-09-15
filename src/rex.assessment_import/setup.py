2db330d46d4ee24fa8d5aba8a59a07db2abbdc18
Oleksiy Golovko <zindel@prometheusresearch.com>
2015-09-15 15:03:04 +0200

    bump version

--- src/rex.assessment_import/setup.py
+++ src/rex.assessment_import/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.assessment_import',
-    version='0.1.1',
+    version='0.2.0',
     description='Assessment Import Tools',
     long_description=open('README.rst', 'r').read(),
     maintainer='Prometheus Research, LLC',

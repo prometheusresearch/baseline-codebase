8d85799f4fc89e9daa9c8f341f8c8e4374147a49
Andrey Popp <8mayday@gmail.com>
2015-09-18 15:53:17 +0300

    feat(port_support): add support mixins for port parameters

---
+++ src/rex.widget/src/rex/widget/port_support.py
@@ -0,0 +1,54 @@
+"""
+
+    rex.widget.port_support
+    =======================
+
+    :copyright: 2015, Prometheus Research, LLC
+
+"""
+
+from contextlib import contextmanager
+
+from werkzeug.local import LocalStack
+from rex.port import Port
+
+__all__ = ('PortSupport',)
+
+_stack = LocalStack()
+_default_parameters = {}
+
+
+def get_parameters():
+    parameters = _stack.top
+    return parameters if parameters is not None else _default_parameters
+
+
+@contextmanager
+def set_parameters(parameters):
+    try:
+        _stack.push(parameters)
+        yield
+    finally:
+        _stack.pop()
+        
+
+class PortSupport(object):
+
+    def __init__(self):
+        super(PortSupport, self).__init__()
+        self.port_parameters = get_parameters()
+
+    @staticmethod
+    def parameters(*args, **kwargs):
+        parameters = {}
+        for arg in args:
+            parameters.update(arg)
+        parameters.update(kwargs)
+        return set_parameters(parameters)
+
+    def create_port(self, port):
+        parameters = [{'parameter': parameter, 'default': default}
+                      for parameter, default
+                      in self.port_parameters.items()]
+        return Port(parameters + [port])
+            

54f92295b56854fe1a4bb139b54a6ca83e70528b
Andrey Popp <8mayday@gmail.com>
2015-09-22 15:11:04 +0500

    Bump rex.action version to 0.5.0

--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -23,7 +23,7 @@ setup(
         'HTSQL==2.3.3.20150901',
         'HTSQL-PGSQL==2.3.3.20150901',
         'COGS==0.4.0',
-        'rex.action==0.4.0',
+        'rex.action==0.5.0',
         'rex.applet==2.2.0',
         'rex.setup==3.1.1',
         'rex.core==1.11.2',

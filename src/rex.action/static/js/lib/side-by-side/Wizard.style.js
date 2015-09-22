ce3eeeae22e179407f1e093d10d1048a6d8407f4
Andrey Popp <8mayday@gmail.com>
2015-09-22 17:30:06 +0500

    fix(side-by-side): 50px for breadcrumb so it can show the context

--- src/rex.action/static/js/lib/side-by-side/Wizard.style.js
+++ src/rex.action/static/js/lib/side-by-side/Wizard.style.js
@@ -17,7 +17,7 @@ export let self = styling({
 
 export let breadcrumb = styling({
   width: '100%',
-  height: 38,
+  height: 50,
   boxShadow: boxShadow(0, 0, 1, 0, rgb(226)),
   borderTop: border(1, borderStyle.solid, rgb(234)),
   borderBottom: border(1, borderStyle.solid, rgb(234))

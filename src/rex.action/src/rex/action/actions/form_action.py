8c32a3c7392b1b732940ba076374cc8fc1abd8ac
Andrey Popp <8mayday@gmail.com>
2015-09-23 14:24:27 +0500

    fix(form_action): infer parameters from complete field list

--- src/rex.action/src/rex/action/actions/form_action.py
+++ src/rex.action/src/rex/action/actions/form_action.py
@@ -7,6 +7,8 @@
 
 """
 
+from cached_property import cached_property
+
 from rex.core import StrVal, AnyVal, MapVal
 from rex.db import Query
 from rex.widget import formfield, dataspec
@@ -46,19 +48,23 @@ class FormAction(EntityAction):
         super(FormAction, self).__init__(**values)
         self.values['use_query'] = self.query is not None
 
+    @cached_property
+    def _complete_fields(self):
+        fields = create_fieldset_from_value(self.value).fields
+        fields = formfield.enrich(fields, self.entity.type.name, db=self.db)
+        fields = fields + self.fields
+        return fields
+
     @responder(
         wrap=lambda self, url: dataspec.EntitySpec(url, self.bind_port()),
         url_type=QueryURL)
     def data_query(self, req):
         query = Query(self.query, self.db)
-        query.parameters = {f.value_key[0]: None for f in self.fields}
+        query.parameters = {f.value_key[0]: None for f in self._complete_fields}
         return query(req)
 
     def create_port(self):
-        fields = create_fieldset_from_value(self.value).fields
-        fields = formfield.enrich(fields, self.entity.type.name, db=self.db)
-        fields = fields + self.fields
-        return super(FormAction, self).create_port(fields=fields)
+        return super(FormAction, self).create_port(fields=self._complete_fields)
 
 
 def create_fieldset_from_value(value, _key=None):

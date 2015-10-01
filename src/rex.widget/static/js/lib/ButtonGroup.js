0193cf71915128d28f0dd7d3d9060c51517e20ac
Andrey Popp <8mayday@gmail.com>
2015-10-01 14:54:31 +0300

    refactor(ButtonGroup): use react-stylesheet

--- src/rex.widget/static/js/lib/ButtonGroup.js
+++ src/rex.widget/static/js/lib/ButtonGroup.js
@@ -2,27 +2,41 @@
  * @copyright 2015, Prometheus Research, LLC
  */
 
-import React  from 'react';
-import Style  from './ButtonGroup.style';
+import React from 'react';
 
-const leftButtonTheme = {self: Style.leftButton};
-const rightButtonTheme = {self: Style.rightButton};
-const middleButtonTheme = {self: Style.middleButton};
+let leftButtonStyle = {
+  borderBottomRightRadius: 0,
+  borderTopRightRadius: 0,
+  borderRight: 'none',
+};
+
+let middleButtonStyle = {
+  borderRadius: 0,
+  borderRight: 'none',
+};
+
+let rightButtonStyle = {
+  borderBottomLeftRadius: 0,
+  borderTopLeftRadius: 0,
+};
+
+function styleElement(element, style) {
+  style = {...element.props.style, ...style};
+  return React.cloneElement(element, {style});
+}
 
 export default class ButtonGroup extends React.Component {
-
-  static defaultTheme = Style;
 
   render() {
     let {children} = this.props;
     let childrenLength = React.Children.count(children);
     children = React.Children.map(children, (button, idx) => {
       if (idx === 0) {
-        return React.cloneElement(button, {theme: leftButtonTheme});
+        return styleElement(button, leftButtonStyle);
       } else if (idx === childrenLength - 1) {
-        return React.cloneElement(button, {theme: rightButtonTheme});
+        return styleElement(button, rightButtonStyle);
       } else {
-        return React.cloneElement(button, {theme: middleButtonTheme});
+        return styleElement(button, middleButtonStyle);
       }
     });
     return (

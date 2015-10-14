0aba9224150a58a90f842922144792786b29a3c8
Andrey Popp <8mayday@gmail.com>
2015-10-14 15:29:16 +0200

    feat(KeyPath): separate keypath utilities module

--- src/rex.widget/static/js/lib/getByKeyPath.js
+++ src/rex.widget/static/js/lib/getByKeyPath.js
@@ -2,20 +2,11 @@
  * @copyright 2015, Prometheus Research, LLC
  */
 
-import isArray from './isArray';
+import * as KeyPath from './KeyPath';
 
 /**
  * Get value by ``keyPath`` within the ``obj``.
  */
 export default function getByKeyPath(obj, keyPath) {
-  if (!isArray(keyPath)) {
-    keyPath = keyPath.split('.').filter(Boolean);
-  }
-  for (let i = 0, len = keyPath.length; i < len; i++) {
-    if (obj == null) {
-      return obj;
-    }
-    obj = obj[keyPath[i]];
-  }
-  return obj;
+  return KeyPath.get(keyPath, obj);
 }

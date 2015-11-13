ce91b135985e1e68a786162f4ba6ad70a2579b4e
Andrey Popp <8mayday@gmail.com>
2015-11-13 13:30:08 +0300

    feat(data): Request.data now allows to make POST XHR

--- src/rex.widget/static/js/lib/data/Request.js
+++ src/rex.widget/static/js/lib/data/Request.js
@@ -2,20 +2,26 @@
  * @copyright 2015, Prometheus Research, LLC
  */
 
-import {fetch} from '../fetch';
+import {fetch, post} from '../fetch';
 import shallowEquals from '../shallowEquals';
 
 export class Request {
 
   static fetch = fetch;
+  static post = post;
 
-  constructor(path, params) {
+  constructor(path, params, data = null) {
     this.path = path;
     this._params = params;
+    this._data = data;
   }
 
   params(params) {
-    return new this.constructor(this.path, {...this._params, ...params});
+    return new this.constructor(this.path, {...this._params, ...params}, this._data);
+  }
+
+  data(data) {
+    return new this.constructor(this.path, this._params, data);
   }
 
   produce(params) {
@@ -23,13 +29,18 @@ export class Request {
       ...this._params,
       ...params
     };
-    return this.constructor.fetch(this.path, query);
+    if (this._data !== null) {
+      return this.constructor.post(this.path, query, this._data);
+    } else {
+      return this.constructor.fetch(this.path, query);
+    }
   }
 
   equals(other) {
     return (
       other &&
       this.constructor === other.constructor &&
+      this._data === other._data &&
       this.path === other.path &&
       shallowEquals(this._params, other._params)
     );
@@ -40,6 +51,6 @@ export function isRequest(obj) {
   return obj instanceof Request;
 }
 
-export default function request(path, params = {}) {
-  return new Request(path, params);
+export default function request(path, params = {}, data = null) {
+  return new Request(path, params, data);
 }

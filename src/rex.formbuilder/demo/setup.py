bcc7227b5be212696c16d34c15d2d362de18df7f
Jason Simeone <jason@prometheusresearch.com>
2015-12-30 16:34:20 -0500

    Fixed demo version

--- src/rex.formbuilder/demo/setup.py
+++ src/rex.formbuilder/demo/setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.formbuilder_demo',
-    version='5.5.1',
+    version='5.6.0',
     description='Demo package for testing rex.formbuilder',
     package_dir={'': 'src'},
     packages=find_packages('src'),

1fdd6655a52fbc7beab7251a9c46c14de6f365c4
Andrey Popp <8mayday@gmail.com>
2016-01-14 18:23:19 +0300

    fix(theme): use brand colors

--- src/rex.widget_chrome/static/js/lib/Theme.js
+++ src/rex.widget_chrome/static/js/lib/Theme.js
@@ -4,32 +4,44 @@
 
 import * as css from 'rex-widget/css';
 
-export default {
-  header: {
-    height: 50,
-    background: '#4490EF',
-    text: '#FFFFFF',
-    textShadow: css.textShadow(0, 1, 0, '#2E5E9A'),
-    boxShadow: css.boxShadow(0, 1, 0, 0, '#578CCE'),
-    hover: {
-      background: '#2873D2',
-    }
-  },
-  headerMenu: {
-    background: '#2873D2',
-    hover: {
-      background: '#3E87E4',
-    }
-  },
-  subHeader: {
-    height: 30,
-    background: '#75B2FF',
-    text: '#FFFFFF',
-    boxShadow: css.boxShadow(0, 1, 0, 0, '#4490EF'),
-    textShadow: css.textShadow(0, 1, 0, '#2E5E9A'),
-    hover: {
-      background: 'radial-gradient(ellipse at 50% 70%, rgb(192, 221, 255) 0%, rgb(117, 178, 255) 70%)',
-
-    },
-  }
+const companyColors = {
+  prometheusDarkBlue: css.color('#004E94'),
+  prometheusLighterBlue: css.color('#0094CD'),
 };
+
+function makeTheme(params) {
+  return {
+    header: {
+      height: 50,
+      background: params.primaryColor.toCSS(),
+      text: '#FFFFFF',
+      boxShadow: css.boxShadow(0, 1, 0, 0, params.primaryColor.darkenByRatio(0.2).toCSS()),
+      textShadow: css.textShadow(0, 1, 0, params.primaryColor.darkenByRatio(0.4).toCSS()),
+      hover: {
+        background: params.primaryColor.lightenByRatio(0.4).toCSS()
+      }
+    },
+    headerMenu: {
+      background: params.primaryColor.lightenByRatio(0.4).toCSS(),
+      hover: {
+        background: params.primaryColor.lightenByRatio(0.5).toCSS(),
+      }
+    },
+    subHeader: {
+      height: 35,
+      background: params.secondaryColor.toCSS(),
+      text: '#FFFFFF',
+      boxShadow: css.boxShadow(0, 1, 0, 0, params.secondaryColor.darkenByRatio(0.2).toCSS()),
+      textShadow: css.textShadow(0, 1, 0, params.secondaryColor.darkenByRatio(0.4).toCSS()),
+      hover: {
+        background: `radial-gradient(ellipse at 50% 55%, ${params.secondaryColor.lightenByRatio(0.6).toCSS()} 0%, ${params.secondaryColor.toCSS()} 70%)`,
+
+      },
+    }
+  };
+}
+
+export default makeTheme({
+  primaryColor: companyColors.prometheusDarkBlue,
+  secondaryColor: companyColors.prometheusLighterBlue,
+});

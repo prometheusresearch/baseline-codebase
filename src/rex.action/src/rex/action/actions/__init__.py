1ddfbaec8b046ea02a2e63447211148c9530f0cf
Andrey Popp <8mayday@gmail.com>
2016-02-17 16:13:33 +0300

    feat: remove actions

--- src/rex.action/src/rex/action/actions/__init__.py
+++ src/rex.action/src/rex/action/actions/__init__.py
@@ -8,12 +8,10 @@
 """
 
 from .view import View
-from .view_cross import ViewCross
 from .make import Make
 from .page import Page
 from .pick import Pick
 from .edit import Edit
-from .edit_cross import EditCross
 from .drop import Drop
 from .form import Form
 from .alternative import Alternative

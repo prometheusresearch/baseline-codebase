7de0f9dc2c92cba08a0606bae087092a346f9eac
Andrey Popp <8mayday@gmail.com>
2016-02-18 15:02:24 +0300

    test: add tests for StringUtils

--- src/rex.action/static/js/lib/__tests__/StringUtils-test.js
+++ src/rex.action/static/js/lib/__tests__/StringUtils-test.js
@@ -2,6 +2,7 @@
  * @copyright 2015, Prometheus Research, LLC
  */
 
+import assert from 'power-assert';
 import * as StringUtils from '../StringUtils';
 
 describe('StringUtils', function() {
@@ -10,16 +11,16 @@ describe('StringUtils', function() {
 
     it('escapes character (,)', function() {
       let escapeComma = StringUtils.makeEscape(',');
-      expect(escapeComma('a')).toEqual('a');
-      expect(escapeComma('a,b')).toEqual('a\\,b');
-      expect(escapeComma('a,b,c')).toEqual('a\\,b\\,c');
+      assert(escapeComma('a') === 'a');
+      assert(escapeComma('a,b') === 'a\\,b');
+      assert(escapeComma('a,b,c') === 'a\\,b\\,c');
     });
 
     it('escapes character (/)', function() {
       let escapeSlash = StringUtils.makeEscape('/');
-      expect(escapeSlash('a')).toEqual('a');
-      expect(escapeSlash('a/b')).toEqual('a\\/b');
-      expect(escapeSlash('a/b/c')).toEqual('a\\/b\\/c');
+      assert(escapeSlash('a') === 'a');
+      assert(escapeSlash('a/b') === 'a\\/b');
+      assert(escapeSlash('a/b/c') === 'a\\/b\\/c');
     });
 
   });
@@ -28,16 +29,16 @@ describe('StringUtils', function() {
 
     it('unescapes character (,)', function() {
       let unescapeComma = StringUtils.makeUnescape(',');
-      expect(unescapeComma('a')).toEqual('a');
-      expect(unescapeComma('a\\,b')).toEqual('a,b');
-      expect(unescapeComma('a\\,b\\,c')).toEqual('a,b,c');
+      assert(unescapeComma('a') === 'a');
+      assert(unescapeComma('a\\,b') === 'a,b');
+      assert(unescapeComma('a\\,b\\,c') === 'a,b,c');
     });
 
     it('unescapes character (/)', function() {
       let unescapeSlash = StringUtils.makeUnescape('/');
-      expect(unescapeSlash('a')).toEqual('a');
-      expect(unescapeSlash('a\\/b')).toEqual('a/b');
-      expect(unescapeSlash('a\\/b\\/c')).toEqual('a/b/c');
+      assert(unescapeSlash('a') === 'a');
+      assert(unescapeSlash('a\\/b') === 'a/b');
+      assert(unescapeSlash('a\\/b\\/c') === 'a/b/c');
     });
 
   });
@@ -49,10 +50,10 @@ describe('StringUtils', function() {
       let escapeComma = StringUtils.makeEscape(',');
       let id = (string) => unescapeComma(escapeComma(string));
 
-      expect(id('a')).toEqual('a');
-      expect(id('a,b')).toEqual('a,b');
-      expect(id(',a,b')).toEqual(',a,b');
-      expect(id('a,b,')).toEqual('a,b,');
+      assert(id('a') === 'a');
+      assert(id('a,b') === 'a,b');
+      assert(id(',a,b') === ',a,b');
+      assert(id('a,b,') === 'a,b,');
     });
 
     it('is idempotent (/)', function() {
@@ -60,10 +61,10 @@ describe('StringUtils', function() {
       let escapeSlash = StringUtils.makeEscape('/');
       let id = (string) => unescapeSlash(escapeSlash(string));
 
-      expect(id('a')).toEqual('a');
-      expect(id('a/b')).toEqual('a/b');
-      expect(id('/a/b')).toEqual('/a/b');
-      expect(id('a/b/')).toEqual('a/b/');
+      assert(id('a') === 'a');
+      assert(id('a/b') === 'a/b');
+      assert(id('/a/b') === '/a/b');
+      assert(id('a/b/') === 'a/b/');
     });
 
   });
@@ -72,20 +73,20 @@ describe('StringUtils', function() {
 
     it('joins array with a character (,)', function() {
       let joinWithComma = StringUtils.makeJoinWith(',');
-      expect(joinWithComma(['a', 'b'])).toEqual('a,b');
-      expect(joinWithComma(['a,b', 'c'])).toEqual('a\\,b,c');
-      expect(joinWithComma(['a,b,c', 'c'])).toEqual('a\\,b\\,c,c');
-      expect(joinWithComma([',a,b,c', 'c'])).toEqual('\\,a\\,b\\,c,c');
-      expect(joinWithComma(['a,b,c,', 'c'])).toEqual('a\\,b\\,c\\,,c');
+      assert(joinWithComma(['a', 'b']) === 'a,b');
+      assert(joinWithComma(['a,b', 'c']) === 'a\\,b,c');
+      assert(joinWithComma(['a,b,c', 'c']) === 'a\\,b\\,c,c');
+      assert(joinWithComma([',a,b,c', 'c']) === '\\,a\\,b\\,c,c');
+      assert(joinWithComma(['a,b,c,', 'c']) === 'a\\,b\\,c\\,,c');
     });
 
     it('joins array with a character (/)', function() {
       let joinWithSlash = StringUtils.makeJoinWith('/');
-      expect(joinWithSlash(['a', 'b'])).toEqual('a/b');
-      expect(joinWithSlash(['a/b', 'c'])).toEqual('a\\/b/c');
-      expect(joinWithSlash(['a/b/c', 'c'])).toEqual('a\\/b\\/c/c');
-      expect(joinWithSlash(['/a/b/c', 'c'])).toEqual('\\/a\\/b\\/c/c');
-      expect(joinWithSlash(['a/b/c/', 'c'])).toEqual('a\\/b\\/c\\//c');
+      assert(joinWithSlash(['a', 'b']) === 'a/b');
+      assert(joinWithSlash(['a/b', 'c']) === 'a\\/b/c');
+      assert(joinWithSlash(['a/b/c', 'c']) === 'a\\/b\\/c/c');
+      assert(joinWithSlash(['/a/b/c', 'c']) === '\\/a\\/b\\/c/c');
+      assert(joinWithSlash(['a/b/c/', 'c']) === 'a\\/b\\/c\\//c');
     });
 
   });
@@ -94,30 +95,30 @@ describe('StringUtils', function() {
 
     it('splits string into an array by character (,)', function() {
       let splitByComma = StringUtils.makeSplitBy(',');
-      expect(splitByComma('a,b')).toEqual(['a', 'b']);
-      expect(splitByComma('a,b,c')).toEqual(['a', 'b', 'c']);
-      expect(splitByComma('a,b,c,d')).toEqual(['a', 'b', 'c', 'd']);
-      expect(splitByComma('a,b,c\\,d')).toEqual(['a', 'b', 'c,d']);
-      expect(splitByComma('a,b\\,c,d')).toEqual(['a', 'b,c', 'd']);
-      expect(splitByComma('a\\,b,c,d')).toEqual(['a,b', 'c', 'd']);
-      expect(splitByComma('\\,a,b,c,d')).toEqual([',a', 'b', 'c', 'd']);
-      expect(splitByComma('a,b,c,d\\,')).toEqual(['a', 'b', 'c', 'd,']);
-      expect(splitByComma('a,b,c,d,')).toEqual(['a', 'b', 'c', 'd', '']);
-      expect(splitByComma(',a,b,c,d')).toEqual(['', 'a', 'b', 'c', 'd']);
+      assert.deepEqual(splitByComma('a,b'),  ['a', 'b']);
+      assert.deepEqual(splitByComma('a,b,c'),  ['a', 'b', 'c']);
+      assert.deepEqual(splitByComma('a,b,c,d'),  ['a', 'b', 'c', 'd']);
+      assert.deepEqual(splitByComma('a,b,c\\,d'),  ['a', 'b', 'c,d']);
+      assert.deepEqual(splitByComma('a,b\\,c,d'),  ['a', 'b,c', 'd']);
+      assert.deepEqual(splitByComma('a\\,b,c,d'),  ['a,b', 'c', 'd']);
+      assert.deepEqual(splitByComma('\\,a,b,c,d'),  [',a', 'b', 'c', 'd']);
+      assert.deepEqual(splitByComma('a,b,c,d\\,'),  ['a', 'b', 'c', 'd,']);
+      assert.deepEqual(splitByComma('a,b,c,d,'),  ['a', 'b', 'c', 'd', '']);
+      assert.deepEqual(splitByComma(',a,b,c,d'),  ['', 'a', 'b', 'c', 'd']);
     });
 
     it('splits string into an array by character (/)', function() {
       let splitBySlash = StringUtils.makeSplitBy('/');
-      expect(splitBySlash('a/b')).toEqual(['a', 'b']);
-      expect(splitBySlash('a/b/c')).toEqual(['a', 'b', 'c']);
-      expect(splitBySlash('a/b/c/d')).toEqual(['a', 'b', 'c', 'd']);
-      expect(splitBySlash('a/b/c\\/d')).toEqual(['a', 'b', 'c/d']);
-      expect(splitBySlash('a/b\\/c/d')).toEqual(['a', 'b/c', 'd']);
-      expect(splitBySlash('a\\/b/c/d')).toEqual(['a/b', 'c', 'd']);
-      expect(splitBySlash('\\/a/b/c/d')).toEqual(['/a', 'b', 'c', 'd']);
-      expect(splitBySlash('a/b/c/d\\/')).toEqual(['a', 'b', 'c', 'd/']);
-      expect(splitBySlash('a/b/c/d/')).toEqual(['a', 'b', 'c', 'd', '']);
-      expect(splitBySlash('/a/b/c/d')).toEqual(['', 'a', 'b', 'c', 'd']);
+      assert.deepEqual(splitBySlash('a/b'),  ['a', 'b']);
+      assert.deepEqual(splitBySlash('a/b/c'),  ['a', 'b', 'c']);
+      assert.deepEqual(splitBySlash('a/b/c/d'),  ['a', 'b', 'c', 'd']);
+      assert.deepEqual(splitBySlash('a/b/c\\/d'),  ['a', 'b', 'c/d']);
+      assert.deepEqual(splitBySlash('a/b\\/c/d'),  ['a', 'b/c', 'd']);
+      assert.deepEqual(splitBySlash('a\\/b/c/d'),  ['a/b', 'c', 'd']);
+      assert.deepEqual(splitBySlash('\\/a/b/c/d'),  ['/a', 'b', 'c', 'd']);
+      assert.deepEqual(splitBySlash('a/b/c/d\\/'),  ['a', 'b', 'c', 'd/']);
+      assert.deepEqual(splitBySlash('a/b/c/d/'),  ['a', 'b', 'c', 'd', '']);
+      assert.deepEqual(splitBySlash('/a/b/c/d'),  ['', 'a', 'b', 'c', 'd']);
     });
 
   });
@@ -129,11 +130,11 @@ describe('StringUtils', function() {
       let joinWithComma = StringUtils.makeJoinWith(',');
       let id = (string) => joinWithComma(splitByComma(string));
 
-      expect(id('a')).toEqual('a');
-      expect(id('a,b')).toEqual('a,b');
-      expect(id(',a,b')).toEqual(',a,b');
-      expect(id('a,b,')).toEqual('a,b,');
-      expect(id('a,b,c')).toEqual('a,b,c');
+      assert(id('a') === 'a');
+      assert(id('a,b') === 'a,b');
+      assert(id(',a,b') === ',a,b');
+      assert(id('a,b,') === 'a,b,');
+      assert(id('a,b,c') === 'a,b,c');
     });
 
     it('is idempotent <-', function() {
@@ -141,11 +142,11 @@ describe('StringUtils', function() {
       let joinWithComma = StringUtils.makeJoinWith(',');
       let id = (string) => splitByComma(joinWithComma(string));
 
-      expect(id(['a'])).toEqual(['a']);
-      expect(id(['a', 'b'])).toEqual(['a', 'b'])
-      expect(id(['', 'a', 'b'])).toEqual(['', 'a', 'b']);
-      expect(id(['', 'a', 'b', ''])).toEqual(['', 'a', 'b', '']);
-      expect(id(['', 'a,b', 'b', ''])).toEqual(['', 'a,b', 'b', '']);
+      assert.deepEqual(id(['a']),  ['a']);
+      assert.deepEqual(id(['a', 'b']),  ['a', 'b'])
+      assert.deepEqual(id(['', 'a', 'b']),  ['', 'a', 'b']);
+      assert.deepEqual(id(['', 'a', 'b', '']),  ['', 'a', 'b', '']);
+      assert.deepEqual(id(['', 'a,b', 'b', '']),  ['', 'a,b', 'b', '']);
     });
 
   });

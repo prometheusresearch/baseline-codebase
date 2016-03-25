9204049781bae13ad85379d2db22405d256a8959
Andrey Popp <8mayday@gmail.com>
2016-03-25 17:47:06 +0500

    fix: fix param binding

--- src/rex.action/static/js/lib/actions/fetchEntity.js
+++ src/rex.action/static/js/lib/actions/fetchEntity.js
@@ -2,9 +2,11 @@
  * @copyright 2016, Prometheus Research, LLC
  */
 
+import {contextToParams} from '../ContextUtils';
+
 export default function fetchEntity({entity, data, context, contextTypes}) {
   let id = context[entity.name].id;
   data = data.params({'*': id}).getSingleEntity();
-  data = data.params(context, contextTypes.input);
+  data = data.params(contextToParams(context, contextTypes.input));
   return {entity: data};
 }

1a2fd9db946f96088a9b8ce9469bb2ed5730b7ef
Andrey Popp <8mayday@gmail.com>
2016-03-28 14:41:31 +0500

    test(form): add tests for <Fieldset />

---
+++ src/rex.widget/static/js/lib/form/__tests__/Fieldset-test.js
@@ -0,0 +1,34 @@
+/**
+ * @copyright 2016, Prometheus Research, LLC
+ */
+
+import React from 'react';
+import {spy, stub, createRenderer, assert} from '../../../testutils';
+
+import Fieldset from '../Fieldset';
+import {Fieldset as FieldsetBase} from 'react-forms';
+
+describe('rex-widget/form', function() {
+
+  describe('<Fieldset />', function() {
+
+    let renderer;
+
+    beforeEach(function() {
+      renderer = createRenderer();
+    });
+
+    it('renders', function() {
+      renderer.render(<Fieldset />);
+      renderer.assertElementWithTypeProps(FieldsetBase);
+      renderer.assertNoElementWithTypeProps('label');
+    });
+
+    it('renders with label', function() {
+      renderer.render(<Fieldset label="Hello" />);
+      renderer.assertElementWithTypeProps(FieldsetBase);
+      renderer.assertElementWithTypeProps('label');
+    });
+  });
+
+})

a6ba5f69e26f7a27792c3b5c4ab29a1f0039aa75
Andrey Popp <8mayday@gmail.com>
2016-03-28 14:07:36 +0500

    Bump rex.setup.

--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -24,7 +24,7 @@ setup(
         'HTSQL-PGSQL==2.3.3.20160326',
         'COGS==0.4.0',
         'rex.action==0.11.0',
-        'rex.setup==3.1.2',
+        'rex.setup==3.1.3',
         'rex.core==1.12.0',
         'rex.ctl==2.1.1',
         'rex.db==3.4.1',

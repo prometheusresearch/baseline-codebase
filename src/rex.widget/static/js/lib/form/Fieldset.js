53c7a6921be76ec4da3e111d2710e70950c0c99c
Andrey Popp <8mayday@gmail.com>
2016-03-28 13:59:18 +0500

    fix(form): make <Fieldset /> handle label prop

--- src/rex.widget/static/js/lib/form/Fieldset.js
+++ src/rex.widget/static/js/lib/form/Fieldset.js
@@ -1,5 +1,25 @@
 /**
- * @copyright 2015, Prometheus Research, LLC
+ * @copyright 2016, Prometheus Research, LLC
  */
 
-export {Fieldset as default} from 'react-forms';
+import React from 'react';
+import {Fieldset as FieldsetBase} from 'react-forms';
+import {VBox} from '../../layout';
+import {style} from '../../stylesheet';
+
+let Label = style('label', {
+  color: '#666',
+  fontSize: '90%',
+  fontWeight: 700,
+  margin: 0,
+  marginBottom: 15,
+});
+
+export default function Fieldset({label, ...props}) {
+  return (
+    <VBox>
+      {label && <Label>{label}</Label>}
+      <FieldsetBase {...props} />
+    </VBox>
+  );
+}

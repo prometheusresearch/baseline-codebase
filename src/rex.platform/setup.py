2c31d2e4f387a3833967c85a913e51a659ef07ee
Andrey Popp <8mayday@gmail.com>
2016-03-31 16:14:39 +0300

    Bump rex.action to 1.0.0

--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -23,7 +23,7 @@ setup(
         'HTSQL==2.3.3.20160326',
         'HTSQL-PGSQL==2.3.3.20160326',
         'COGS==0.4.0',
-        'rex.action==0.11.0',
+        'rex.action==1.0.0',
         'rex.setup==3.1.3',
         'rex.core==1.12.1',
         'rex.ctl==2.1.1',

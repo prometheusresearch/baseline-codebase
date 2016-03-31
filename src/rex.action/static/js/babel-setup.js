f721640def3f893ac2396a8e6e683f7f0ad7ffe4
Andrey Popp <8mayday@gmail.com>
2016-03-31 10:43:45 +0000

    merge

---
+++ src/rex.action/static/js/babel-setup.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var path = require('path');
+
+var REX_ACTION = /rex[\.\-]action(\/static)?\/js\/lib/;
+var REX_WIDGET = /rex[\.\-]widget(\/static)?\/js\/lib/;
+
+var config = JSON.parse(fs.readFileSync(path.join(process.cwd(), '.babelrc')));
+config.only = function(filename) {
+  var ok = REX_ACTION.exec(filename) || REX_WIDGET.exec(filename);
+  return ok;
+};
+
+require('babel/register')(config);

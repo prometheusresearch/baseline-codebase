85ea93b767fb59b5400f3923c0cc4aebde464c96
Andrey Popp <8mayday@gmail.com>
2016-04-11 16:48:32 +0300

    fix(ui): wrap buttons in a toolbar

--- src/rex.action/static/js/lib/wizard/Toolbar.js
+++ src/rex.action/static/js/lib/wizard/Toolbar.js
@@ -12,27 +12,29 @@ import groupBy from '../groupArrayBy';
 import {getTitleAtNode} from '../ActionTitle';
 import {getIconAtNode} from '../ActionIcon';
 
-export function ToolbarButton({node, onClick, groupHorizontally}) {
-  let Button;
-  switch (node.element.props.kind) {
-    case 'success':
-      Button = ui.SuccessButton;
-      break;
-    case 'danger':
-      Button = ui.DangerButton;
-      break;
-    default:
-      Button = ui.Button;
-  }
+export function ToolbarButton({node, attach, onClick, groupHorizontally}) {
+  let Button = buttonForNode(node);
   return (
     <Button
       size="small"
       onClick={onClick.bind(null, node.keyPath)}
       icon={getIconAtNode(node)}
+      attach={attach}
       variant={{groupHorizontally}}>
       {getTitleAtNode(node)}
     </Button>
   );
+}
+
+function buttonForNode(node) {
+  switch (node.element.props.kind) {
+    case 'success':
+      return ui.SuccessButton;
+    case 'danger':
+      return ui.DangerButton;
+    default:
+      return ui.Button;
+  }
 }
 
 export default function Toolbar({graph, onClick}) {
@@ -40,16 +42,36 @@ export default function Toolbar({graph, onClick}) {
     graph.nextActions().filter(node => !Instruction.Replace.is(node.instruction)),
     node => node.element.props.kind);
   let buttonGroups = nodes
-    .map((nodes, idx) =>
-      <layout.HBox marginRight={5} marginBottom={5} key={idx}>
-        {nodes.map(node =>
-          <ToolbarButton
-            key={node.keyPath}
-            node={node}
-            groupHorizontally={nodes.length > 1}
-            onClick={onClick}
-            />)}
-      </layout.HBox>
-    );
+    .map((nodes, idx) => {
+      let borderLeft = '1px solid #eee';
+      if (nodes.length > 0) {
+        let button = buttonForNode(nodes[0]);
+        if (
+          button.stylesheet &&
+          button.stylesheet.Root &&
+          button.stylesheet.Root.stylesheet &&
+          button.stylesheet.Root.stylesheet.style
+        ) {
+          borderLeft = button.stylesheet.Root.stylesheet.style.base.border;
+        }
+      }
+      return (
+        <layout.HBox
+          key={idx}
+          style={{borderLeft}}
+          wrap="wrap"
+          marginRight={5}
+          marginBottom={5}
+          maxWidth="100%">
+          {nodes.map(node =>
+            <ToolbarButton
+              key={node.keyPath}
+              node={node}
+              attach={{left: true}}
+              onClick={onClick}
+              />)}
+        </layout.HBox>
+      )
+    });
   return <layout.HBox wrap="wrap">{buttonGroups}</layout.HBox>;
 }

9fd4a07ae0a5d741f9c715b321791a5c2b8e9871
Kirill Simonov <xi@resolvent.net>
2016-04-12 16:03:31 -0500

    HTSQL==2.3.3.20160412

--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -20,8 +20,8 @@ setup(
     url='https://bitbucket.org/rexdb/rex.platform',
     include_package_data=True,
     install_requires=[
-        'HTSQL==2.3.3.20160405',
-        'HTSQL-PGSQL==2.3.3.20160405',
+        'HTSQL==2.3.3.20160412',
+        'HTSQL-PGSQL==2.3.3.20160412',
         'COGS==0.4.0',
         'rex.action==1.0.1',
         'rex.setup==3.1.4',

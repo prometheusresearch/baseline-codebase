849cb13446c1588ecaa9924237ea1b9cc81451a0
Andrey Popp <8mayday@gmail.com>
2016-04-12 14:16:17 +0300

    feat(ui): <Breadcrumb /> now collapses items after some threshold

--- src/rex.action/static/js/lib/wizard/Breadcrumb.js
+++ src/rex.action/static/js/lib/wizard/Breadcrumb.js
@@ -4,7 +4,7 @@
 
 import React from 'react';
 
-import * as Stylesheet from 'rex-widget/stylesheet';
+import * as stylesheet from 'rex-widget/stylesheet';
 import * as layout  from 'rex-widget/layout';
 import * as ui from 'rex-widget/ui';
 import * as css from 'rex-widget/css';
@@ -12,11 +12,41 @@ import * as css from 'rex-widget/css';
 import ActionTitle from '../ActionTitle';
 import {getIconAtNode} from '../ActionIcon';
 
-let BreadcrumbButtonWrapper = Stylesheet.style(layout.HBox, {
-  paddingLeft: 15
+let BreadcrumbButtonWrapper = stylesheet.style(layout.HBox, {
+  paddingLeft: 15,
+  maxWidth: '20%',
 });
 
-export let BreadcrumbButton = Stylesheet.style(ui.ButtonBase, {
+let BreadcrumbTriangle = stylesheet.style('div', {
+  content: '',
+  position: css.position.absolute,
+  top: 21,
+  left: '100%',
+  height: 0,
+  width: 0,
+  border: css.border(5, 'transparent'),
+  borderRightWidth: 0,
+  borderLeftWidth: 5,
+
+  zIndex: 2,
+  borderLeftColor: 'inherit',
+
+  first: {
+    left: 'calc(100% + 2px)',
+    borderLeftColor: css.rgb(100)
+  },
+
+  second: {
+    borderLeftColor: css.rgb(255)
+  },
+});
+
+let BreadcrumbRoot = stylesheet.style(layout.HBox, {
+  background: css.rgb(255),
+  boxShadow: css.boxShadow(0, 1, 1, 0, css.rgb(204)),
+});
+
+export let BreadcrumbButton = stylesheet.style(ui.ButtonBase, {
   Root: {
     fontSize: '85%',
     fontWeight: 700,
@@ -45,43 +75,39 @@ export let BreadcrumbButton = Stylesheet.style(ui.ButtonBase, {
   }
 });
 
-let BreadcrumbTriangle = Stylesheet.style('div', {
-  content: '',
-  position: css.position.absolute,
-  top: 21,
-  left: '100%',
-  height: 0,
-  width: 0,
-  border: css.border(5, 'transparent'),
-  borderRightWidth: 0,
-  borderLeftWidth: 5,
+export class Breadcrumb extends React.Component {
 
-  zIndex: 2,
-  borderLeftColor: 'inherit',
-});
-
-let BreadcrumbRoot = Stylesheet.style(layout.HBox, {
-  background: css.rgb(255),
-  boxShadow: css.boxShadow(0, 1, 1, 0, css.rgb(204)),
-});
-
-export default function Breadcrumb({graph, onClick}) {
-  let style = {
-    left: 'calc(100% + 2px)',
-    borderLeftColor: css.rgb(100)
+  static defaultProps = {
+    collapseAfter: 4,
   };
-  let buttons = graph.trace.slice(1, -1).map(node =>
-    <BreadcrumbButtonWrapper key={node.keyPath}>
-      <BreadcrumbButton
-        onClick={onClick.bind(null, node.keyPath)}
-        icon={getIconAtNode(node)}>
-        <ActionTitle noWrap node={node} />
-      </BreadcrumbButton>
-      <BreadcrumbTriangle style={style} />
-      <BreadcrumbTriangle
-        style={{borderLeftColor: css.rgb(255)}}
-        />
-    </BreadcrumbButtonWrapper>
-  );
-  return <BreadcrumbRoot>{buttons}</BreadcrumbRoot>;
+
+  render() {
+    let {graph} = this.props;
+    let nodes = graph.trace.slice(1, -1);
+    let buttons = nodes.map(this.renderButton, this);
+    return <BreadcrumbRoot>{buttons}</BreadcrumbRoot>;
+  }
+
+  renderButton(node, idx, nodes) {
+    let {onClick, collapseAfter} = this.props;
+    let showTitle = (
+      nodes.length <= collapseAfter ||
+      nodes.length > collapseAfter && nodes.length - idx <= collapseAfter
+    );
+    return (
+      <BreadcrumbButtonWrapper key={node.keyPath}>
+        <BreadcrumbButton
+          onClick={onClick.bind(null, node.keyPath)}
+          icon={getIconAtNode(node)}>
+          {showTitle
+            ? <ActionTitle noWrap node={node} />
+            : null}
+        </BreadcrumbButton>
+        <BreadcrumbTriangle variant={{first: true}} />
+        <BreadcrumbTriangle variant={{second: true}} />
+      </BreadcrumbButtonWrapper>
+    );
+  }
 }
+
+export default Breadcrumb;

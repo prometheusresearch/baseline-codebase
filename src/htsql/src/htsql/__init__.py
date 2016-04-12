d83cbe44ba1264960912c4c010f842c174d2ad81
Kirill Simonov <xi@resolvent.net>
2016-04-12 17:43:37 -0500

    Bumped the version number; removed dependency on setuptools.

--- src/htsql/src/htsql/__init__.py
+++ src/htsql/src/htsql/__init__.py
@@ -47,7 +47,7 @@ To execute a raw HTSQL request, run::
 """
 
 
-__version__ = '2.3.3.20160405'
+__version__ = '2.3.3.20160412'
 __copyright__ = """Copyright (c) 2006-2016, Prometheus Research, LLC"""
 __license__ = """
 This is free software: you are free to change and redistribute it.

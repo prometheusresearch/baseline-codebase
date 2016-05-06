d403ba911cf523d25ec8a7fa674ca5cec8c98a8f
Andrey Popp <8mayday@gmail.com>
2016-05-06 17:30:46 +0300

    Bump own version and rex.widget version

--- src/rex.platform/demo/setup.py
+++ src/rex.platform/demo/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.platform_demo',
-    version='5.1.1',
+    version='5.2.0',
     description="Demo package for testing rex.platform",
     include_package_data=True,
     install_requires=[

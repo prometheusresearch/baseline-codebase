75d52965e4c772af9801a43eaecc3304cddfdb10
Oleksiy Golovko <zindel@prometheusresearch.com>
2016-05-26 13:33:32 +0200

    add basic actions

--- src/rex.dbgui/src/dbgui/wizard.py
+++ src/rex.dbgui/src/dbgui/wizard.py
@@ -4,36 +4,61 @@ from rex.action.action import ActionVal
 from rex.action.wizard import Wizard
 
 action_val = ActionVal()
-omap = OMapVal()
-
-def pick(table):
-    return action_val(dict(
-        type='pick',
-        id='pick-%s' % table,
-        entity=table,
-        title='Pick %s' % table
-    ))
-
-def view(table):
-    return action_val(dict(
-        type='view',
-        id='view-%s' % table,
-        entity=table,
-        title='View %s' % table
-    ))
+wizard_val = ActionVal(action_class=Wizard)
 
 @cached
-def get_wizard(table):
-    return ActionVal(action_class=Wizard)(dict(
-        type='wizard',
-        id='wizard-%s' % table,
-        path=[
-            omap([
-                ('pick-individual', [omap([('view-individual', None)])])
-            ])
-        ],
-        actions={
-            'pick-individual': pick(table),
-            'view-individual': view(table),
-        }
-    ))
+def get_wizard(table_name):
+    wizard = table(table_name)
+    flatten = lambda l: reduce(lambda a, b: a + flatten(b)
+                               if isinstance(b, (list, tuple))
+                               else a + [b], l, [])
+    actions = dict([(a.id, a) for a in flatten(wizard)
+                    if a and not isinstance(a, (str, unicode))])
+    path = get_path(wizard)
+    return action(table_name, type='wizard', actions=actions, path=path)
+
+def get_path(wizard):
+    dict1 = lambda x,y: dict([(x, y)])
+    if wizard is None:
+        return None
+    elif isinstance(wizard, (str, unicode)):
+        return [{'replace': wizard}]
+    else:
+        return [dict1(k.id, get_path(v)) for k, v in wizard]
+
+def action(table_name, type, **kwds):
+    constructor = wizard_val if type == 'wizard' else action_val
+    kwds['type'] = type
+    kwds['id'] = '%s-%s' % (type, table_name)
+    if type != 'wizard':
+        kwds['entity'] = table_name
+    return constructor(kwds)
+
+def table(table_name):
+    [(pick_action, _)] = pick(table_name)
+    [(make_action, _)] = make(table_name)
+    return [(pick_action, record(table_name) + [
+        (make_action, '../..' + replace(table_name))
+    ])]
+
+def replace(table_name, with_pick=True):
+    ret = '/view-%s' % table_name
+    if with_pick:
+        ret = ('/pick-%s' % table_name) + ret
+    return ret
+
+def pick(table_name):
+    return [(
+        action(table_name, type='pick'),
+    None)]
+
+def make(table_name):
+    return [(
+        action(table_name, type='make'),
+    None)]
+
+def record(table_name):
+    #TODO: generate fields replacing entity with links
+    view = action(table_name, type='view')
+    edit = action(table_name, type='edit')
+    return [(view, [(edit, '../../..' + replace(table_name))])]

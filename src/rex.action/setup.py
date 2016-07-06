c4e429823ace810ab8c22b838d657e5f6dcc1e25
Andrey Popp <8mayday@gmail.com>
2016-07-06 12:44:02 +0300

    chore: bump version

--- src/rex.action/setup.py
+++ src/rex.action/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.action',
-    version='1.1.0',
+    version='1.2.0',
     description="Foundation of the RexDB platform",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

5d9f4a6aff48325803fffa4416d49a96e1f47b39
Andrey Popp <8mayday@gmail.com>
2016-07-25 11:55:14 +0300

    Bump version

--- src/rex.setup/demo/setup.py
+++ src/rex.setup/demo/setup.py
@@ -22,7 +22,7 @@ class demo(Command):
 setup(
     name='rex.setup_demo',
     description="This package demonstrates capabilities of rex.setup",
-    version='3.1.4',
+    version='3.2.0',
     maintainer="Prometheus Research, LLC",
     maintainer_email="contact@prometheusresearch.com",
     url="http://bitbucket.org/prometheus/rex.setup",
--- src/rex.setup/setup.py
+++ src/rex.setup/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.setup',
-    version='3.1.4',
+    version='3.2.0',
     description="Distutils extension for the RexDB platform",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

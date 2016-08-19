f09f8dfe4403f6a00d22c6c5dca5458977ed3fdf
Andrey Popp <8mayday@gmail.com>
2016-08-19 12:34:18 +0300

    Merged in rm-slots (pull request #3)
    Rm slots

--- src/rex.about/setup.py
+++ src/rex.about/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.about',
-    version='0.2.0',
+    version='0.3.0',
     description='A RexDB applet for displaying application versions and'
     ' licensing information',
     long_description=open('README.rst', 'r').read(),

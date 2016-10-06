8fbdc7d316de4cbaa68bd1745368794c1352d8f3
Jason Simeone <jason@prometheusresearch.com>
2016-10-06 15:14:39 -0400

    Quck hack to let us specify a different API endpoint

--- src/rex.query/demo/static/js/src/index.js
+++ src/rex.query/demo/static/js/src/index.js
@@ -2,9 +2,15 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as qs from 'qs';
 import QueryBuilderApp from './QueryBuilderApp';
 
+
+let params = qs.parse(window.location.search.substr(1));
+
+
 ReactDOM.render(
-  <QueryBuilderApp />,
+  <QueryBuilderApp api={params.api} />,
   document.getElementById('root')
 );
+

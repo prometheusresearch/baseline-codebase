7def3fdd62850099524d250c4066995dd7342fa4
Andrey Popp <8mayday@gmail.com>
2016-10-07 16:53:45 +0300

    chore: bump version

--- src/rex.widget/setup.py
+++ src/rex.widget/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.widget',
-    version='2.9.1',
+    version='2.9.2',
     description="Widget toolkit for the RexDB platform",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

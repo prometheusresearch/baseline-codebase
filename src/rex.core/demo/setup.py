3d31c131513605a11d93894c05ab0b38393b8655
Kirill Simonov <xi@resolvent.net>
2016-10-11 14:02:45 -0500

    Bumped the version number of the demo package.

--- src/rex.core/demo/setup.py
+++ src/rex.core/demo/setup.py
@@ -23,7 +23,7 @@ class demo(Command):
 
 setup(
     name='rex.core_demo',
-    version = "1.13.1",
+    version = "1.14.0",
     description="Demo package for testing rex.core",
     package_dir={'': 'src'},
     packages=find_packages('src'),

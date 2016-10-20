3ff788e74c59ff6360e31b7c25fc8698da65524c
Eugenia Rudenko <sirius@prometheusresearch.com>
2016-10-20 16:24:46 +0000

    Merged in study-1894 (pull request #14)
    added check if assessment_import_dir exists and accessible

--- src/rex.assessment_import/src/rex/assessment_import/__init__.py
+++ src/rex.assessment_import/src/rex/assessment_import/__init__.py
@@ -1,1 +1,26 @@
+import os
+
+from rex.core import get_settings, Initialize, Error
+
+
 from .settings import *
+
+class InitializeAssessmentImport(Initialize):
+    # Verifies that `assessment_import_dir` is a valid directory and writable.
+
+    def __call__(self):
+        settings = get_settings()
+        assessment_import_dir = settings.assessment_import_dir
+        if assessment_import_dir is None:
+            return
+        if not os.path.isdir(assessment_import_dir):
+            raise Error(
+                "Asessment import storage (%s) does not exist:" \
+                % AssessmentImportDir.name,
+                assessment_import_dir)
+        if not os.access(assessment_import_dir, os.R_OK|os.W_OK|os.X_OK):
+            raise Error(
+                "Asessment import storage (%s) is not accessible:" \
+                % AssessmentImportDir.name,
+                assessment_import_dir)
+

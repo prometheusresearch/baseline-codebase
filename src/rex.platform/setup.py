a6d53487b4902a0efd38dd5e9334dbab3c95026e
Oleksiy Golovko <zindel@prometheusresearch.com>
2016-10-28 13:31:06 +0200

    bump version

--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='rex.platform',
-    version='5.6.0',
+    version='6.0.0',
     description='RexDB Core Platform Libraries',
     long_description=open('README.rst', 'r').read(),
     author='Prometheus Research, LLC',

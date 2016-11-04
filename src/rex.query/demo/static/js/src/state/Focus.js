78817869c21a865d517eef917d2fdcb082f9e6ad
Andrey Popp <8mayday@gmail.com>
2016-11-04 12:48:27 +0300

    fix(ui): fix focus management
    This isn't ideal yet and we want it to be refactored soon. At least it works
    more consistently now.

--- src/rex.query/demo/static/js/src/state/Focus.js
+++ src/rex.query/demo/static/js/src/state/Focus.js
@@ -2,14 +2,21 @@
  * @flow
  */
 
-import type {Query} from '../model';
+import type {Query, SelectQuery, QueryPipeline} from '../model';
 
 import * as ArrayUtil from '../ArrayUtil';
-import * as q from '../model/Query';
+import {
+  inferQueryType,
+  transformQuery,
+  flattenPipeline
+} from '../model/Query';
+import {
+  maybeAtom
+} from '../model/Type';
 
 export type Focus = Array<string>;
 
-export function chooseFocus(query: Query): Focus {
+export function chooseFocus(query: QueryPipeline): Focus {
   let focusList = getFocuses(query);
   let lengths = focusList.map(f => f.length);
   let max = ArrayUtil.max(lengths);
@@ -17,40 +24,95 @@ export function chooseFocus(query: Query): Focus {
   return focusList[idx];
 }
 
-function getFocuses(query: Query): Array<Focus> {
-  return getFocusesImpl(query, [], false);
+function getFocuses(query: QueryPipeline): Array<Focus> {
+  let focusList = getPipelineFocusList(query, [], {}, false);
+  return focusList;
 }
 
-function getFocusesImpl(query: Query, path: Array<string>, suppressPath: boolean) {
-  switch (query.name) {
-    case 'pipeline': {
-      let result: Array<Array<string>> = [];
-      let pipeline = q.flattenPipeline(query).pipeline;
-      let localPath = [];
-      for (let i = 0; i < pipeline.length; i++) {
-        if (pipeline[i].name === 'navigate' && !suppressPath) {
-          localPath = pipeline[i].path;
-        }
-        result = result.concat(getFocusesImpl(pipeline[i], path.concat(localPath), false));
+function getPipelineFocusList(
+  query: QueryPipeline,
+  path: Array<string>,
+  defineMap: {[name: string]: QueryPipeline},
+  suppressPath: boolean
+) {
+  let result: Array<Array<string>> = [];
+  let type = maybeAtom(query.context.type);
+  if (type == null) {
+    return [];
+  } else if (type.name === 'record' || type.name === 'entity') {
+    // TODO: Remove flattenPipeline, make sure it's not needed.
+    let pipeline = flattenPipeline(query).pipeline;
+    let localPath = [];
+    for (let i = 0; i < pipeline.length; i++) {
+      let item = pipeline[i];
+      if (item.name === 'define') {
+        defineMap = {
+          ...defineMap,
+          // XXX: This needs to be fixed to use more stable identifiers for define
+          // bindings. Currently shadowing isn't allowed and this is a bug.
+          [item.binding.name]: item.binding.query,
+        };
       }
-      return result;
-    }
-    case 'aggregate':
-      return [];
-    case 'navigate': {
-      let type = query.context.type;
-      return type && type.name === 'seq' ? [path] : [];
-    }
-    case 'select': {
-      let result: Array<Focus> = [];
-      for (let k in query.select) {
-        if (query.select.hasOwnProperty(k)) {
-          result = result.concat(getFocusesImpl(query.select[k], path.concat(k), true));
-        }
+      if (item.name === 'navigate' && !suppressPath) {
+        localPath = item.path;
       }
-      return result;
+      result = result.concat(
+        getQueryFocusList(
+          item,
+          path.concat(localPath),
+          defineMap,
+          false
+        )
+      );
     }
-    default:
-      return [];
+    return result;
+  } else {
+    return [];
   }
+}
+
+function getSelectFocusList(
+  query: SelectQuery,
+  path: Array<string>,
+  defineMap: {[name: string]: QueryPipeline},
+  suppressPath: boolean
+) {
+  let result: Array<Focus> = [];
+  for (let k in query.select) {
+    if (!query.select.hasOwnProperty(k)) {
+      continue;
+    }
+    let item = query.select[k];
+    if (defineMap[k] != null) {
+      item = inferQueryType(query.context.prev, defineMap[k]);
+    }
+    result = result.concat(
+      getPipelineFocusList(
+        item,
+        path.concat(k),
+        defineMap,
+        true
+      )
+    );
+  }
+  return result;
+}
+
+function getQueryFocusList(
+  query: Query,
+  path: Array<string>,
+  defineMap: {[name: string]: QueryPipeline},
+  suppressPath: boolean
+) {
+  return transformQuery(query, {
+    pipeline: _ => getPipelineFocusList(...arguments),
+    select: _ => getSelectFocusList(...arguments),
+    navigate: query => {
+      let type = query.context.type;
+      return type && type.name === 'seq'
+        ? [path]
+        : [];
+    },
+    otherwise: _query => [],
+  });
 }

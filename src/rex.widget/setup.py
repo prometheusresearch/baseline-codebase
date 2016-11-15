d6ba0dbb11dffc2e37c27f4df18ec5bae6e71330
Oleksiy Golovko <zindel@prometheusresearch.com>
2016-11-15 13:26:14 +0100

    chore: bump version

--- src/rex.widget/setup.py
+++ src/rex.widget/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.widget',
-    version='2.10.0',
+    version='2.11.0',
     description="Widget toolkit for the RexDB platform",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

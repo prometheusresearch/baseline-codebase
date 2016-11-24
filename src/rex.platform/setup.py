18de14286aa85c410e7ff3e9a5fa8c4f25ab82b5
Oleksiy Golovko <zindel@prometheusresearch.com>
2016-11-24 13:13:56 +0100

    bump dbgui

--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -42,7 +42,7 @@ setup(
         'rex.widget==2.11.0',
         'rex.widget_chrome==1.2.3',
         'rex.file==1.0.4',
-        'rex.dbgui==4.1.2',
+        'rex.dbgui==4.1.3',
     ],
     rex_static='static',
     rex_bundle={

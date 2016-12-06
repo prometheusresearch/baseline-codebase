11a896f22bcf6e9ddff8f3ccd38583ef3a9df73c
Jason Simeone <jason@prometheusresearch.com>
2016-12-06 15:42:36 -0500

    Fixed invalid test data

--- src/rex.instrument/setup.py
+++ src/rex.instrument/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.instrument',
-    version='1.7.0',
+    version='1.7.1',
     description='Class interfaces and framework for using EDC components',
     long_description=open('README.rst', 'r').read(),
     author='Prometheus Research, LLC',

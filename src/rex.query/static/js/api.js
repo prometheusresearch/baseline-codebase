61829b755b666e998734d6c11a5fdb4d0683a0e7
Andrey Popp <8mayday@gmail.com>
2016-12-09 15:22:39 +0300

    feat(ui): add rex-query/api module

---
+++ src/rex.query/static/js/api.js
@@ -0,0 +1,12 @@
+/**
+ * @flow
+ */
+
+var q = require('./src/model/Query');
+var QueryBuilderApp = require('./src/QueryBuilderApp').default;
+
+module.exports = {
+  QueryBuilderApp: QueryBuilderApp,
+  serializeQuery: q.serializeQuery,
+  deserializeQuery: q.deserializeQuery
+};

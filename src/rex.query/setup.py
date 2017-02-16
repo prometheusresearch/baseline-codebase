407496f42e959268647aed0af675d616510c08b0
Andrey Popp <8mayday@gmail.com>
2017-02-16 12:08:10 +0300

    chore: bump version

--- src/rex.query/setup.py
+++ src/rex.query/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.query',
-    version = "0.3.1",
+    version = "0.4.0",
     description="Compositional database query interface",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

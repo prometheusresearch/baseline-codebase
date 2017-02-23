d045de008b9bc560e032f761d30fe58e0bfb0ad0
Oleksiy Golovko <zindel@prometheusresearch.com>
2017-02-23 10:26:57 +0100

    bump version to 6.2.0

--- src/rex.platform/demo/setup.py
+++ src/rex.platform/demo/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.platform_demo',
-    version='6.1.2',
+    version='6.2.0',
     description="Demo package for testing rex.platform",
     include_package_data=True,
     install_requires=[
--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='rex.platform',
-    version='6.1.2',
+    version='6.2.0',
     description='RexDB Core Platform Libraries',
     long_description=open('README.rst', 'r').read(),
     author='Prometheus Research, LLC',

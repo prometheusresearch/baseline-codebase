c4fe46d6ef194c95d7a374b731c30ee2cf063f4d
Andrey Popp <8mayday@gmail.com>
2017-06-12 12:21:51 +0300

    Bump rex.widget to 3.0.0

--- src/rex.tabular_import/setup.py
+++ src/rex.tabular_import/setup.py
@@ -36,7 +36,7 @@ setup(
         'rex.asynctask>=0.5,<0.7',
         'rex.attach>=2.0.4,<3',
         'rex.file>=1.0.4,<2',
-        'rex.widget>=2.11,<3',
+        'rex.widget>=3,<4',
         'tablib>=0.10,<0.11',
     ],
     rex_init='rex.tabular_import',

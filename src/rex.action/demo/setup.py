b3a0ed84a23b58706366e33e2ba838334b15112d
Andrey Popp <8mayday@gmail.com>
2017-06-23 13:59:30 +0300

    chore: bump version to 1.6.1

--- src/rex.action/demo/setup.py
+++ src/rex.action/demo/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.action_demo',
-    version='1.6.0',
+    version='1.6.1',
     description="Demo package for testing rex.action",
     package_dir={'': 'src'},
     packages=find_packages('src'),
--- src/rex.action/setup.py
+++ src/rex.action/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.action',
-    version='1.6.0',
+    version='1.6.1',
     description="Foundation of the RexDB platform",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

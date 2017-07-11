54aa81243ef973f4800e16f4cc88c79114626b81
Jason Simeone <jason@prometheusresearch.com>
2017-07-11 13:35:31 -0400

    Version bump

--- src/rex.mart_actions/setup.py
+++ src/rex.mart_actions/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.mart_actions',
-    version='0.7.0',
+    version='0.7.1',
     description='A collection of RexAction actions and wizards for exploring'
     'RexMart databases.',
     long_description=open('README.rst', 'r').read(),

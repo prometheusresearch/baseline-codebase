06e2986ddc56194afe8875b2c0b747947ed8b16b
Andrey Popp <8mayday@gmail.com>
2017-08-02 15:17:04 +0300

    Bump version: 6.3.0

--- src/rex.platform/demo/setup.py
+++ src/rex.platform/demo/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.platform_demo',
-    version='6.2.2',
+    version='6.3.0',
     description="Demo package for testing rex.platform",
     include_package_data=True,
     install_requires=[

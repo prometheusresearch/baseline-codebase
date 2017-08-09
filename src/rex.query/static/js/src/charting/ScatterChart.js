5460345dde0ad41e2ba29babc16934ad2dba048c
Andrey Popp <8mayday@gmail.com>
2017-08-09 14:14:16 +0300

    fix: wrap <ScatterChart /> into <div /> to fix styling

--- src/rex.query/static/js/src/charting/ScatterChart.js
+++ src/rex.query/static/js/src/charting/ScatterChart.js
@@ -28,21 +28,22 @@ export default function ScatterChart({
     rendered = <NoDataMessage />;
   } else if (chart.xColumn != null && chart.yColumn != null) {
     rendered = (
-      <recharts.ScatterChart
-        width={600}
-        height={400}
-        margin={{top: 50, right: 30, left: 20, bottom: 5}}
-        style={{fontWeight: 200, fontSize: '9pt'}}>
-        <g>
-          <ChartTitle left={300} value={label} onChange={onLabel} />
-        </g>
-        <recharts.XAxis dataKey={chart.xColumn} name={chart.xLabel} />
-        <recharts.YAxis dataKey={chart.yColumn} name={chart.yLabel} />
-        <recharts.CartesianGrid strokeDasharray="3 3" />
-        <recharts.Tooltip cursor={{strokeDasharray: '3 3'}} />
-        <recharts.Legend />
-        <recharts.Scatter data={data} fill="#8884d8" />
-      </recharts.ScatterChart>
+      <div style={{fontWeight: 200, fontSize: '9pt'}}>
+        <recharts.ScatterChart
+          width={600}
+          height={400}
+          margin={{top: 50, right: 30, left: 20, bottom: 5}}>
+          <g>
+            <ChartTitle left={300} value={label} onChange={onLabel} />
+          </g>
+          <recharts.XAxis dataKey={chart.xColumn} name={chart.xLabel} />
+          <recharts.YAxis dataKey={chart.yColumn} name={chart.yLabel} />
+          <recharts.CartesianGrid strokeDasharray="3 3" />
+          <recharts.Tooltip cursor={{strokeDasharray: '3 3'}} />
+          <recharts.Legend />
+          <recharts.Scatter data={data} fill="#8884d8" />
+        </recharts.ScatterChart>
+      </div>
     );
   }
   return (

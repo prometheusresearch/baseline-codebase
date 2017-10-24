8b25177ca9c1a8857c538fb3ebdc45492e11935c
Andrey Popp <8mayday@gmail.com>
2017-10-24 21:35:55 +0300

    Bump version

--- src/rex.forms/demo/setup.py
+++ src/rex.forms/demo/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.forms_demo',
-    version='2.4.0',
+    version='2.4.1',
     description='Demo package for testing rex.forms',
     package_dir={'': 'src'},
     packages=find_packages('src'),

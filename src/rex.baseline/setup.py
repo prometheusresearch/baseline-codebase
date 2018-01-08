773c6c8716163e4411fc5b79552e4ad8b92d1444
Kirill Simonov <xi@resolvent.net>
2018-01-08 09:05:00 -0600

    Updated to the latest rex.platform.

--- src/rex.baseline/setup.py
+++ src/rex.baseline/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='rex.baseline',
-    version='1.1.0',
+    version='1.2.0',
     description='Baseline for RexDB applications',
     long_description=open('README.rst', 'r').read(),
     maintainer='Prometheus Research, LLC',
@@ -17,7 +17,7 @@ setup(
     url='https://bitbucket.org/rexdb/rex.baseline',
     include_package_data=True,
     install_requires=[
-        'rex.platform >= 6.2, < 6.3',
+        'rex.platform >= 6.3, < 6.4',
         'rex.query >= 0.3.1, < 2',
         'rex.asynctask >= 0.5.0, < 2',
     ],

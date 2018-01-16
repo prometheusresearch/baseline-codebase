722f06f2065129a90e53d918d7ab0f7345d82e89
Andrey Popp <8mayday@gmail.com>
2018-01-16 15:24:20 +0300

    Bump dev version

--- src/rex.widget/demo/setup.py
+++ src/rex.widget/demo/setup.py
@@ -22,7 +22,7 @@ class demo(Command):
 
 setup(
     name='rex.widget_demo',
-    version='3.1.0',
+    version='3.1.1',
     description="Demo package for testing rex.widget",
     namespace_packages=['rex'],
     package_dir={'': 'src'},
--- src/rex.widget/setup.py
+++ src/rex.widget/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.widget',
-    version='3.1.0',
+    version='3.1.1',
     description="Widget toolkit for the RexDB platform",
     long_description=open('README.rst', 'r').read(),
     maintainer="Prometheus Research, LLC",

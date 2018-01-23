8addbad647458f067373c5799eb0a36c5113e175
Oleksiy Golovko <zindel@prometheusresearch.com>
2018-01-23 18:24:01 +0100

    merge

--- src/rex.platform/setup.py
+++ src/rex.platform/setup.py
@@ -22,7 +22,7 @@ setup(
         'HTSQL-PGSQL==2.3.3.20171027',
         'COGS==0.4.1',
         'rex.action==1.7.0',
-        'rex.setup==4.1.1',
+        'rex.setup==4.1.2',
         'rex.core==1.17.0',
         'rex.ctl==2.3.0',
         'rex.db==3.7.0',

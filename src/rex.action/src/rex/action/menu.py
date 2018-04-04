1268c01c17d27b945ee37962e58861d50f60788f
Oleksiy Golovko <zindel@prometheusresearch.com>
2018-04-04 15:44:34 +0200

    add rex_action_validate_on_startup setting

--- src/rex.action/src/rex/action/menu.py
+++ src/rex.action/src/rex/action/menu.py
@@ -15,7 +15,7 @@ from rex.menu import Menu
 from rex.web import PathMask
 
 from .render import ActionRenderer
-from .mount import MountedActionVal
+from .mount import MountedAction, MountedActionVal
 
 __all__ = ('ActionMenu',)
 
@@ -27,9 +27,13 @@ class ActionMenu(Menu):
 
     def __init__(self, path, access, value):
         super(ActionMenu, self).__init__(path, access, value)
-        self.renderer = ActionRenderer(self.masks(), self.value, self.access, None)
+        self.renderer = None
 
     def render(self, req):
+        if not isinstance(self.value, MountedAction):
+            self.value = self.value()
+        if self.renderer == None:
+            self.renderer = ActionRenderer(self.masks(), self.value, self.access, None)
         return self.renderer(req)
 
     def masks(self):

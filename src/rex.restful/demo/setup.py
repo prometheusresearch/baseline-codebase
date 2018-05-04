d64af3768c7a46c426c21780c5ffc47b5c64cb23
Jason Simeone <jason@prometheusresearch.com>
2018-05-04 10:26:53 -0400

    Synced up other restful repo

--- src/rex.restful/demo/setup.py
+++ src/rex.restful/demo/setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rex.restful_demo',
-    version='1.3.0',
+    version='1.3.1',
     description='Demo package for testing rex.restful',
     package_dir={'': 'src'},
     packages=find_packages('src'),

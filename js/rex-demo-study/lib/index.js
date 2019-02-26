1c50ad49b63ac5dc995c8357f2298b506b9621bc
Jason Simeone <jason@prometheusresearch.com>
2019-02-26 14:38:57 -0500

    Removed unnecessary study package

--- js/rex-demo-study/lib/index.js
+++
@@ -1,4 +0,0 @@
-export { default as BigTextInput } from "./BigTextInput";
-export { default as CustomGuideChart } from "./CustomGuideChart";
-export { default as customVQBChart } from "./customVQBChart";
-export { default as BuildTableMart } from "./BuildTableMart";

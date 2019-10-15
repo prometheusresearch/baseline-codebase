1e01caead6f6de3dd207650bf597d7518b07e502
Andrey Popp <8mayday@gmail.com>
2019-10-15 12:03:51 +0300

    rapid: updates to deps and test data

--- js/rex-demo-ui/src/index.js
+++ js/rex-demo-ui/src/index.js
@@ -3,12 +3,43 @@
 import invariant from "invariant";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import * as RexGraphQL from "rex-graphql";
+import {
+  defineQuery,
+  unstable_useResource as useResource
+} from "rex-graphql/Resource";
 
-function App() {
-  return <div>Hello</div>;
+function WithResource({ endpoint, render: Render }) {
+  let resource = React.useMemo(
+    () =>
+      defineQuery<void, any>({
+        endpoint,
+        query: `
+        query {
+          __schema {
+            types { name }
+          }
+        }
+      `
+      }),
+    [endpoint]
+  );
+  return <Render resource={resource} />;
+}
+
+function App({ resource }) {
+  let data = useResource(resource);
+  return <div>{JSON.stringify(data, null, 2)}</div>;
 }
 
 let root = document.getElementById("root");
 invariant(root != null, "DOM is not avaialble: missing #root");
 
-ReactDOM.render(<App />, root);
+let endpoint = RexGraphQL.configure("/_api/graphql");
+
+ReactDOM.render(
+  <React.Suspense fallback={<div>Loading...</div>}>
+    <WithResource endpoint={endpoint} render={App} />
+  </React.Suspense>,
+  root
+);

09b461b0f1cfa06533a3f92f372f0b1f40c46414
Andrey Popp <8mayday@gmail.com>
2019-11-21 18:36:01 +0300

    rex-demo-ui: improve ShowUser

--- js/rex-demo-ui/ShowUser.js
+++ js/rex-demo-ui/ShowUser.js
@@ -2,6 +2,8 @@
 
 import * as React from "react";
 import * as Router from "rex-ui/Router";
+import * as mui from "@material-ui/core";
+import * as routes from "./index.js";
 
 export let screen: Router.ShowScreen = {
   type: "show",
@@ -17,10 +19,52 @@ export let screen: Router.ShowScreen = {
         field: "contact_info",
         require: [{ field: "id" }, { field: "type" }, { field: "value" }],
       },
-      render: ({ value }) => JSON.stringify(value),
+      render: ContactInfoList,
+    },
+    {
+      title: "Sites",
+      require: {
+        field: "sites",
+        require: [
+          {
+            field: "site",
+            require: [{ field: "id" }, { field: "title" }],
+          },
+          { field: "role" },
+        ],
+      },
+      render: SiteList,
     },
   ],
   RenderTitle: props => {
     return props.data.remote_user;
   },
 };
+
+function ContactInfoList(props) {
+  let items = props.value.map(item => {
+    return (
+      <mui.ListItem>
+        <mui.ListItemText primary={item.value} secondary={item.type} />
+      </mui.ListItem>
+    );
+  });
+  return <mui.List>{items}</mui.List>;
+}
+
+function SiteList(props) {
+  let items = props.value.map(item => {
+    let onClick = () => {
+      routes.router.push(routes.showSite, { id: item.site.id });
+    };
+    return (
+      <mui.ListItem button={true} onClick={onClick}>
+        <mui.ListItemText
+          primary={item.site.title}
+          secondary={item.role}
+        />
+      </mui.ListItem>
+    );
+  });
+  return <mui.List>{items}</mui.List>;
+}

935fdcfb00129808c7ceafb58ea42cb4a995db59
Vladimir Khapalov <khapaloff@gmail.com>
2020-02-26 18:08:11 +0300

    rapid: use new resource in select

--- js/rex-ui/rapid/Select.js
+++ js/rex-ui/rapid/Select.js
@@ -6,26 +6,29 @@ import invariant from "invariant";
 import * as React from "react";
 
 import { type Endpoint } from "rex-graphql";
-import * as Resource from "rex-graphql/Resource";
+import * as Resource from "rex-graphql/Resource2";
 import * as mui from "@material-ui/core";
 
 import { introspect } from "./Introspection";
 import * as EndpointSchemaStorage from "./EndpointSchemaStorage.js";
 import * as QueryPath from "./QueryPath.js";
-import * as Field from "./FieldLegacy.js";
+import * as FieldLegacy from "./FieldLegacy.js";
+import * as Field from "./Field.js";
+import * as Filter from "./Filter.js";
 
-export type SelectProps = {|
+export type SelectProps<V, R, O = *> = {|
   /** GraphQL endpoint. */
   endpoint: Endpoint,
-  /** Path inside GraphQL schema. */
-  fetch: string,
+  /** Resource to get data from GraphQL. */
+  resource: Resource.Resource<V, R>,
+  getRows: R => O,
 
   /** Field which specifies the label. */
-  labelField: Field.FieldConfig,
+  labelField: Field.FieldConfig<>,
   /** Field which specifies the id. */
-  idField?: Field.FieldConfig,
+  idField?: Field.FieldConfig<>,
   /** Additional fields to query. */
-  fields?: { [name: string]: Field.FieldConfig },
+  fields?: Array<Field.FieldConfig<>>,
 
   /** Currently selected value. */
   value: ?string,
@@ -33,65 +36,72 @@ export type SelectProps = {|
   onValue: (?string) => void,
 |};
 
-export function Select(props: SelectProps) {
+export function Select<V, R>(props: SelectProps<V, R>) {
   let {
-    fetch,
+    resource,
+    getRows,
     endpoint,
     labelField,
     idField = "id",
-    fields = {},
+    fields = [],
     value,
     onValue,
   } = props;
-  let schema = EndpointSchemaStorage.useIntrospectionSchema(endpoint);
 
-  let { resource, path, fieldSpecs } = React.useMemo(() => {
-    let path = QueryPath.make(fetch);
-    let { query, fieldSpecs } = introspect({
-      schema,
-      path,
-      fields: { ...fields, id: idField, label: labelField },
-    });
-    let resource = Resource.defineQuery<void, any>({ endpoint, query });
-    return { path, resource, fieldSpecs };
-  }, [fetch, endpoint, schema, labelField, fields, idField]);
+  let fieldSpecs = Field.configureFields(fields);
+
+  let idFieldSpec = Field.configureField(idField);
+  let labelFieldSpec = Field.configureField(labelField);
 
   return (
     <SelectRenderer
-      path={path}
+      endpoint={endpoint}
       resource={resource}
+      getRows={getRows}
       fieldSpecs={fieldSpecs}
+      idFieldSpec={idFieldSpec}
+      labelFieldSpec={labelFieldSpec}
       value={value}
       onValue={onValue}
     />
   );
 }
 
-type SelectRendererProps = {|
-  path: QueryPath.QueryPath,
-  resource: Resource.Resource<any, any>,
-  fieldSpecs: { id: Field.FieldSpec, label: Field.FieldSpec },
+type SelectRendererProps<V, R, O = *> = {|
+  endpoint: Endpoint,
+  resource: Resource.Resource<V, R>,
+  getRows: R => O,
+  fieldSpecs: Array<Field.FieldSpec>,
+  idFieldSpec: Field.FieldSpec,
+  labelFieldSpec: Field.FieldSpec,
   value: ?string,
   onValue: (?string) => void,
 |};
 
-function SelectRenderer({
-  path,
+function SelectRenderer<V, R>({
+  endpoint,
   resource,
+  getRows,
   fieldSpecs,
+  idFieldSpec,
+  labelFieldSpec,
   value,
   onValue,
-}: SelectRendererProps) {
-  let data = Resource.unstable_useResource(resource);
-  for (let key of QueryPath.toArray(path)) {
-    if (data == null) {
-      break;
-    }
-    data = data[key];
+}: SelectRendererProps<V, R>) {
+  let [isFetching, resourceData] = Resource.useResource(
+    endpoint,
+    resource,
+    ({}: any),
+  );
+
+  if (isFetching || resourceData == null) {
+    return null;
   }
+
+  let data = getRows(resourceData);
   let items = data.map(item => {
-    let id = item[fieldSpecs.id.require.field];
-    let label = item[fieldSpecs.label.require.field];
+    let id = item[idFieldSpec.name];
+    let label = item[labelFieldSpec.name];
     return (
       <mui.MenuItem key={id} value={id}>
         {label}

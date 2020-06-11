7eb6724f2309e953442329af46300625b6be7ad2
Konstantin Ryzhov <theaklair@gmail.com>
2020-06-11 14:45:41 +0300

    js: jest config updated; added sample ts jest test; updated Makefile

--- js/rex-conf-jest/jest.config.js
+++ js/rex-conf-jest/jest.config.js
@@ -1,26 +1,30 @@
 module.exports = {
-  collectCoverageFrom: ["**/*.js"],
+  collectCoverageFrom: ["**/*.js", "**/*.ts"],
   // TODO: this is neede for pnp, enable when needed.
   // resolver: "jest-pnp-resolver",
   setupFiles: ["react-app-polyfill/jsdom"],
   testMatch: [
     "<rootDir>/**/__tests__/**/*-test.{js,jsx}",
-    "<rootDir>/**/*.(spec|test).{js,jsx}"
+    "<rootDir>/**/*.(spec|test).{js,jsx}",
+    "<rootDir>/**/__tests__/**/*-test.{ts,tsx}",
+    "<rootDir>/**/*.(spec|test).{ts,tsx}",
   ],
   testEnvironment: "jsdom",
   testURL: "http://localhost",
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
     "^.+\\.css$": require.resolve("./cssTransform.js"),
-    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": require.resolve("./fileTransform.js")
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": require.resolve(
+      "./fileTransform.js",
+    ),
   },
-//transformIgnorePatterns: [
-//  "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$",
-//  "^.+\\.module\\.(css|sass|scss)$"
-//],
+  //transformIgnorePatterns: [
+  //  "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|ts|tsx)$",
+  //  "^.+\\.module\\.(css|sass|scss)$"
+  //],
   moduleNameMapper: {
     "^react-native$": "react-native-web",
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy"
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
   },
   moduleFileExtensions: [
     "web.js",
@@ -32,6 +36,6 @@ module.exports = {
     "json",
     "web.jsx",
     "jsx",
-    "node"
-  ]
+    "node",
+  ],
 };
